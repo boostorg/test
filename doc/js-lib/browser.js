a087d7e72f63862f24af22c241394c71295bbb59
Gennadiy Rozental <rogeeff@gmail.com>
2002-09-10 07:42:41 +0000

    Javascript removed HTML validity check passed
    [SVN r15249]

--- doc/js-lib/browser.js
+++
@@ -1,6 +0,0 @@
-// Determine browser.
-
-var ie  = document.all && navigator.userAgent.indexOf("Opera")==-1;
-var dom = document.getElementById&&!ie&&navigator.userAgent.indexOf("Opera")==-1;
-
-// EOF
--- doc/js-lib/viso_core.js
+++
@@ -1,3 +0,0 @@
-JS.include( 'callback_api.js', viso_path );
-JS.include( 'scheduler_api.js', viso_path );
-JS.include( 'vis_object_api.js', viso_path );
