fc5f093cd474c99e2ffb70cc0f343523679da6cf
Gennadiy Rozental <rogeeff@gmail.com>
2002-09-12 08:28:30 +0000

    tabs cleaned
    [SVN r15274]

--- test/minimal_test.cpp
+++ test/minimal_test.cpp
@@ -22,7 +22,7 @@ struct bool_convertible1 {
     bool_convertible1( bool v ) : v_( v ) {}
     operator bool() { return v_; }
 
-	bool v_;
+    bool v_;
 };
 
 //____________________________________________________________________________//
@@ -31,7 +31,7 @@ struct bool_convertible2 {
     bool_convertible2( int v ) : v_( v ) {}
     operator int() { return v_; }
 
-	int v_;
+    int v_;
 };
 
 //____________________________________________________________________________//
@@ -39,10 +39,10 @@ struct bool_convertible2 {
 struct bool_convertible3 {
     bool_convertible3( void* v ) : v_( v ) {}
 
-	struct Tester {};
+    struct Tester {};
     operator Tester*() { return (Tester*)v_; }
 
-	void* v_;
+    void* v_;
 };
 
 //____________________________________________________________________________//
@@ -50,24 +50,24 @@ struct bool_convertible3 {
 int 
 test_main( int argc, char* argv[] ) 
 {
-	int i = 1;
-	BOOST_CHECK( i == 1 );
-	BOOST_CHECK( i == 2 );
+    int i = 1;
 
-	BOOST_CHECK( bool_convertible1( true  ) );
-	BOOST_CHECK( bool_convertible1( false ) );
+    BOOST_CHECK( i == 1 );
+    BOOST_CHECK( i == 2 );
 
-	BOOST_CHECK( bool_convertible2( 1 ) );
-	BOOST_CHECK( bool_convertible2( 0 ) );
+    BOOST_CHECK( bool_convertible1( true  ) );
+    BOOST_CHECK( bool_convertible1( false ) );
 
-	BOOST_CHECK( bool_convertible3( (void*)1 ) );
-	BOOST_CHECK( bool_convertible3( NULL ) );
+    BOOST_CHECK( bool_convertible2( 1 ) );
+    BOOST_CHECK( bool_convertible2( 0 ) );
 
-	
-	BOOST_ERROR( "Some error" );
+    BOOST_CHECK( bool_convertible3( (void*)1 ) );
+    BOOST_CHECK( bool_convertible3( NULL ) );
 
-	BOOST_REQUIRE( i == 4 );
-	
+    BOOST_ERROR( "Some error" );
+
+    BOOST_REQUIRE( i == 4 );
+
     return 0;
 }
 
@@ -77,6 +77,9 @@ test_main( int argc, char* argv[] )
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.2  2002/09/12 08:28:30  rogeeff
+//  tabs cleaned
+//
 //  Revision 1.1  2002/09/04 07:23:32  rogeeff
 //  minimal testiing unit test added
 //
