32160019515607139b22f218c6569f11a717f7c9
Gennadiy Rozental <rogeeff@gmail.com>
2002-09-16 06:30:40 +0000

    switch to use new tools names
    [SVN r15364]

--- example/test_exec_example.cpp
+++ example/test_exec_example.cpp
@@ -7,12 +7,18 @@ int add( int i, int j ) { return i+j; }
 int test_main( int, char *[] )  // note the name!
 {
   // six ways to detect and report the same error:
-  BOOST_TEST( add(2,2) == 4 );           // #1 continues on error
-  BOOST_CRITICAL_TEST( add(2,2) == 4 );  // #2 throws on error
+  BOOST_CHECK( add(2,2) == 4 );          // #1 continues on error
+
+  BOOST_REQUIRE( add(2,2) == 4 );        // #2 throws on error
+
   if ( add(2,2) != 4 )
     BOOST_ERROR( "Ouch...");             // #3 continues on error
+
   if ( add(2,2) != 4 )
-    BOOST_CRITICAL_ERROR( "Ouch..." );   // #4 throws on error
-  if ( add(2,2) != 4 ) throw "Oops...";  // #5 throws on error
+    BOOST_FAIL( "Ouch..." );             // #4 throws on error
+
+  if ( add(2,2) != 4 ) 
+    throw "Oops...";                     // #5 throws on error
+
   return add(2,2) == 4 ? 0 : 1;          // #6 returns error code
 }
