7fecf6a272459c3b9915909c5020cde9d1fce9b6
Gennadiy Rozental <rogeeff@gmail.com>
2002-11-03 03:06:16 +0000

    wrapstream constructor issue fix revisited
    [SVN r16079]

--- include/boost/test/test_tools.hpp
+++ include/boost/test/test_tools.hpp
@@ -44,52 +44,52 @@
 
 #define BOOST_CHECKPOINT(message_) \
     boost::test_toolbox::detail::checkpoint_impl( \
-        boost::test_toolbox::detail::wrapstrstream() << message_, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << message_, __FILE__, __LINE__)
 
 #define BOOST_WARN(predicate) \
     boost::test_toolbox::detail::warn_and_continue_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << #predicate, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << #predicate, __FILE__, __LINE__)
 
 #define BOOST_CHECK(predicate) \
     boost::test_toolbox::detail::test_and_continue_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << #predicate, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << #predicate, __FILE__, __LINE__)
 
 #define BOOST_CHECK_EQUAL(left_, right_) \
     boost::test_toolbox::detail::equal_and_continue_impl((left_), (right_), \
-        boost::test_toolbox::detail::wrapstrstream() << #left_ " == " #right_, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << #left_ " == " #right_, __FILE__, __LINE__)
 
 #define BOOST_CHECK_CLOSE(left_, right_, tolerance_src) \
     boost::test_toolbox::detail::compare_and_continue_impl((left_), (right_), (tolerance_src),\
-        boost::test_toolbox::detail::wrapstrstream() << #left_ " ~= " #right_, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << #left_ " ~= " #right_, __FILE__, __LINE__)
 
 #define BOOST_REQUIRE(predicate) \
     boost::test_toolbox::detail::test_and_throw_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << #predicate, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << #predicate, __FILE__, __LINE__)
 
 #define BOOST_MESSAGE(message_) \
     boost::test_toolbox::detail::message_impl( \
-        boost::test_toolbox::detail::wrapstrstream() << message_, __FILE__, __LINE__)
+        boost::test_toolbox::detail::wrapstrstream().ref() << message_, __FILE__, __LINE__)
 
 #define BOOST_WARN_MESSAGE(predicate, message_) \
     boost::test_toolbox::detail::warn_and_continue_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << message_, __FILE__, __LINE__,false)
+        boost::test_toolbox::detail::wrapstrstream().ref() << message_, __FILE__, __LINE__,false)
 
 #define BOOST_CHECK_MESSAGE(predicate, message_) \
     boost::test_toolbox::detail::test_and_continue_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << message_, __FILE__, __LINE__,false)
+        boost::test_toolbox::detail::wrapstrstream().ref() << message_, __FILE__, __LINE__,false)
 
 #define BOOST_REQUIRE_MESSAGE(predicate, message_) \
     boost::test_toolbox::detail::test_and_throw_impl((predicate), \
-        boost::test_toolbox::detail::wrapstrstream() << message_, __FILE__, __LINE__,false)
+        boost::test_toolbox::detail::wrapstrstream().ref() << message_, __FILE__, __LINE__,false)
 
 #define BOOST_CHECK_PREDICATE( predicate, arg_list_size, arg_list ) \
     boost::test_toolbox::detail::test_and_continue_impl(predicate, BOOST_PLACE_PREDICATE_ARGS ## arg_list_size arg_list, \
-        boost::test_toolbox::detail::wrapstrstream() << #predicate << "("\
+        boost::test_toolbox::detail::wrapstrstream().ref() << #predicate << "("\
         << BOOST_PRINT_PREDICATE_ARGS ## arg_list_size arg_list << ")", __FILE__, __LINE__)
 
 #define BOOST_REQUIRE_PREDICATE( predicate, arg_list_size, arg_list ) \
     boost::test_toolbox::detail::test_and_throw_impl(predicate, BOOST_PLACE_PREDICATE_ARGS ## arg_list_size arg_list, \
-        boost::test_toolbox::detail::wrapstrstream() << #predicate << "("\
+        boost::test_toolbox::detail::wrapstrstream().ref() << #predicate << "("\
         << BOOST_PRINT_PREDICATE_ARGS ## arg_list_size arg_list << ")", __FILE__, __LINE__)
 
 #define BOOST_ERROR(message_) BOOST_CHECK_MESSAGE( false, message_ )
@@ -104,7 +104,7 @@
 
 #define BOOST_CHECK_EQUAL_COLLECTIONS(left_begin_, left_end_, right_begin_) \
     boost::test_toolbox::detail::equal_and_continue_impl( (left_begin_), (left_end_), (right_begin_),\
-        boost::test_toolbox::detail::wrapstrstream() << \
+        boost::test_toolbox::detail::wrapstrstream().ref() << \
             "{" #left_begin_ ", " #left_end_ "}" " == {" #right_begin_ ", ...}", __FILE__, __LINE__)
 
 #define BOOST_IS_DEFINED(symb) boost::test_toolbox::detail::is_defined_impl( #symb, BOOST_STRINGIZE(=symb) )
@@ -142,28 +142,27 @@ typedef std::ostringstream  out_stringstream;
 // ************************************************************************** //
 
 struct wrapstrstream {
-    mutable boost::shared_ptr<out_stringstream> m_buf;
-    mutable std::string         m_str;
+    out_stringstream            m_buf;
+    std::string                 m_str;
 
-    wrapstrstream() : m_buf( new out_stringstream ) {}
-
-    std::string const&          str() const;
+    wrapstrstream&              ref() { return *this; }
+    std::string const&          str();
 };
 
 //____________________________________________________________________________//
 
 template <class T>
-inline wrapstrstream const&
-operator<<( wrapstrstream const& targ, T const& t )
+inline wrapstrstream&
+operator<<( wrapstrstream& targ, T const& t )
 {
-    *(targ.m_buf) << t;
+    targ.m_buf << t;
     return targ;
 }
 
 //____________________________________________________________________________//
 
-inline wrapstrstream const&
-operator<<( wrapstrstream const& targ, wrapstrstream const& src )
+inline wrapstrstream&
+operator<<( wrapstrstream& targ, wrapstrstream& src )
 {
     targ << src.str();
     return targ;
@@ -209,26 +208,26 @@ private:
 // ************************************************************************** //
 
 void
-checkpoint_impl( wrapstrstream const& message_, char const* file_name_, int line_num_ );
+checkpoint_impl( wrapstrstream& message_, char const* file_name_, int line_num_ );
 
 //____________________________________________________________________________//
 
 void
-message_impl( wrapstrstream const& message_, char const* file_name_, int line_num_ );
+message_impl( wrapstrstream& message_, char const* file_name_, int line_num_ );
 
 //____________________________________________________________________________//
 
 // ************************************* //
 
 void
-warn_and_continue_impl( bool predicate_, wrapstrstream const& message_,
+warn_and_continue_impl( bool predicate_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true );
 
 //____________________________________________________________________________//
 
 void
-warn_and_continue_impl( extended_predicate_value const& v_, wrapstrstream const& message_,
+warn_and_continue_impl( extended_predicate_value const& v_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true );
 
@@ -237,12 +236,12 @@ warn_and_continue_impl( extended_predicate_value const& v_, wrapstrstream const&
 // ************************************* //
 
 bool  // return true if error detected
-test_and_continue_impl( bool predicate_, wrapstrstream const& message_,
+test_and_continue_impl( bool predicate_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors );
 void
-test_and_throw_impl   ( bool predicate_, wrapstrstream const& message_,
+test_and_throw_impl   ( bool predicate_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_fatal_errors );
@@ -250,7 +249,7 @@ test_and_throw_impl   ( bool predicate_, wrapstrstream const& message_,
 //____________________________________________________________________________//
 
 bool
-test_and_continue_impl( extended_predicate_value const& v_, wrapstrstream const& message_,
+test_and_continue_impl( extended_predicate_value const& v_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors );
@@ -260,7 +259,7 @@ test_and_continue_impl( extended_predicate_value const& v_, wrapstrstream const&
 // Borland bug workaround
 #if defined(__BORLANDC__) && (__BORLANDC__ < 0x560)
 bool
-test_and_continue_impl( void* ptr, wrapstrstream const& message_,
+test_and_continue_impl( void* ptr, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
@@ -272,7 +271,7 @@ test_and_continue_impl( void* ptr, wrapstrstream const& message_,
 //____________________________________________________________________________//
 
 void
-test_and_throw_impl   ( extended_predicate_value const& v_, wrapstrstream const& message_,
+test_and_throw_impl   ( extended_predicate_value const& v_, wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         bool add_fail_pass_ = true,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_fatal_errors );
@@ -282,7 +281,7 @@ test_and_throw_impl   ( extended_predicate_value const& v_, wrapstrstream const&
 template<typename ArgType, typename Predicate>
 inline bool
 test_and_continue_impl( Predicate const& pred_, ArgType const& arg_,
-                        wrapstrstream const& message_,
+                        wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
 {
@@ -290,7 +289,7 @@ test_and_continue_impl( Predicate const& pred_, ArgType const& arg_,
 
     if( !predicate ) {
         return test_and_continue_impl( predicate,
-                                       wrapstrstream() << "test " << message_ << " failed for " << arg_,
+                                       wrapstrstream().ref() << "test " << message_ << " failed for " << arg_,
                                        file_name_, line_num_, false, log_level_ );
     }
 
@@ -302,7 +301,7 @@ test_and_continue_impl( Predicate const& pred_, ArgType const& arg_,
 template<typename ArgType, typename Predicate>
 inline void
 test_and_throw_impl   ( Predicate const& pred_, ArgType const& arg_,
-                        wrapstrstream const& message_,
+                        wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_fatal_errors )
 {
@@ -316,7 +315,7 @@ test_and_throw_impl   ( Predicate const& pred_, ArgType const& arg_,
 template<typename First, typename Second, typename Predicate>
 inline bool
 test_and_continue_impl( Predicate const& pred_, First const& first_, Second const& second_,
-                        wrapstrstream const& message_,
+                        wrapstrstream& message_,
                         char const* file_name_, int line_num_,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
 {
@@ -324,7 +323,7 @@ test_and_continue_impl( Predicate const& pred_, First const& first_, Second cons
 
     if( !predicate ) {
         return test_and_continue_impl( predicate,
-            wrapstrstream() << "test " << message_ << " failed for (" << first_ << ", " << second_ << ")",
+            wrapstrstream().ref() << "test " << message_ << " failed for (" << first_ << ", " << second_ << ")",
             file_name_, line_num_, false, log_level_ );
     }
 
@@ -336,7 +335,7 @@ test_and_continue_impl( Predicate const& pred_, First const& first_, Second cons
 template<typename First, typename Second, typename Predicate>
 inline void
 test_and_throw_impl   ( First const& first_, Second const& second_, Predicate const& pred_,
-                        wrapstrstream const& message_, char const* file_name_, int line_num_,
+                        wrapstrstream& message_, char const* file_name_, int line_num_,
                         unit_test_framework::report_level log_level_ = unit_test_framework::report_fatal_errors )
 {
     if( test_and_continue_impl( first_, second_, pred_, message_, file_name_, line_num_, log_level_ ) ) {
@@ -349,7 +348,7 @@ test_and_throw_impl   ( First const& first_, Second const& second_, Predicate co
 // ************************************* //
 
 bool
-equal_and_continue_impl( char const* left_, char const* right_, wrapstrstream const& message_,
+equal_and_continue_impl( char const* left_, char const* right_, wrapstrstream& message_,
                          char const* file_name_, int line_num_,
                          unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors );
 
@@ -358,14 +357,14 @@ equal_and_continue_impl( char const* left_, char const* right_, wrapstrstream co
 template <class Left, class Right>
 inline bool
 equal_and_continue_impl( Left const& left_, Right const& right_,
-                         wrapstrstream const& message_, char const* file_name_, int line_num_,
+                         wrapstrstream& message_, char const* file_name_, int line_num_,
                          unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
 {
     bool predicate = (left_ == right_);
 
     if( !predicate ) {
         return test_and_continue_impl( predicate,
-            wrapstrstream() << "test " << message_
+            wrapstrstream().ref() << "test " << message_
                             << " failed [" << left_ << " != " << right_ << "]",
             file_name_, line_num_, false, log_level_ );
     }
@@ -378,7 +377,7 @@ equal_and_continue_impl( Left const& left_, Right const& right_,
 template <class Left, class Right>
 inline void
 equal_and_continue_impl( Left left_begin_, Left left_end_, Right right_begin_,
-                         wrapstrstream const& message_,
+                         wrapstrstream& message_,
                          char const* file_name_, int line_num_,
                          unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
 {
@@ -393,7 +392,7 @@ equal_and_continue_impl( Left left_begin_, Left left_end_, Right right_begin_,
 template<typename FPT, typename ToleranceSource>
 inline bool
 compare_and_continue_impl( FPT left_, FPT right_, ToleranceSource tolerance_src,
-                           wrapstrstream const& message_,
+                           wrapstrstream& message_,
                            char const* file_name_, int line_num_,
                            unit_test_framework::report_level log_level_ = unit_test_framework::report_all_errors )
 {
@@ -401,7 +400,7 @@ compare_and_continue_impl( FPT left_, FPT right_, ToleranceSource tolerance_src,
 
     if( !predicate ) {
         return test_and_continue_impl( predicate,
-            wrapstrstream() << "test " << message_
+            wrapstrstream().ref() << "test " << message_
                             << " failed [" << left_ << " !~= " << right_
                             << " (+/-" << compute_tolerance( tolerance_src, left_ ) << ")]",
             file_name_, line_num_, false, log_level_ );
@@ -463,6 +462,9 @@ private:
 //  Revision History :
 //
 //  $Log$
+//  Revision 1.23  2002/11/03 03:06:16  rogeeff
+//  wrapstream constructor issue fix revisited
+//
 //  Revision 1.22  2002/11/02 19:31:04  rogeeff
 //  merged into the main trank
 //
