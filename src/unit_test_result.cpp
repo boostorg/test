072e7bfcf0b9849370b4d9e49f8731194b7f6264
Gennadiy Rozental <rogeeff@gmail.com>
2002-12-08 18:16:57 +0000

    eliminated all uses of NULL switched to use c_string_literal
    [SVN r16549]

--- src/unit_test_result.cpp
+++ src/unit_test_result.cpp
@@ -39,38 +39,43 @@ namespace boost {
 
 namespace unit_test_framework {
 
+typedef unit_test_result* unit_test_result_ptr;
+
 // ************************************************************************** //
 // **************               unit_test_result               ************** //
 // ************************************************************************** //
 
 struct unit_test_result::Impl {
-    unit_test_result*               m_parent;
-    std::list<unit_test_result*>    m_children;
+    unit_test_result_ptr            m_parent;
+    std::list<unit_test_result_ptr> m_children;
     unit_test_counter               m_assertions_passed;
     unit_test_counter               m_assertions_failed;
     unit_test_counter               m_expected_failures;
     unit_test_counter               m_test_cases_passed;
     unit_test_counter               m_test_cases_failed;
     bool                            m_exception_caught;
-    char const*                     m_test_case_name;
+    c_string_literal                m_test_case_name;
 
     static boost::scoped_ptr<unit_test_result> m_head;
-    static unit_test_result*        m_curr;
+    static unit_test_result_ptr     m_curr;
 
     bool                            is_failed()   { return m_assertions_failed != m_expected_failures || m_exception_caught; }
-    int                             result_code() { return is_failed() 
-                                                        ? ( (m_assertions_failed != 0) 
-                                                               ? boost::exit_test_failure 
-                                                               : boost::exit_exception_failure )
-                                                        : boost::exit_success; }
+    int                             result_code()
+    { 
+        return is_failed() 
+                    ? ( (m_assertions_failed != 0) 
+                            ? boost::exit_test_failure 
+                            : boost::exit_exception_failure )
+                    : boost::exit_success; 
+    }
 };
 
 boost::scoped_ptr<unit_test_result> unit_test_result::Impl::m_head;
-unit_test_result* unit_test_result::Impl::m_curr = NULL;
+unit_test_result_ptr unit_test_result::Impl::m_curr = unit_test_result_ptr();
 
 //____________________________________________________________________________//
 
-unit_test_result::unit_test_result( unit_test_result* parent, char const* test_case_name, unit_test_counter exp_fail )
+unit_test_result::unit_test_result( unit_test_result_ptr parent, c_string_literal test_case_name, unit_test_counter exp_fail )
 : m_pimpl( new Impl )
 {
     m_pimpl->m_parent            = parent;
@@ -87,7 +92,7 @@ unit_test_result::unit_test_result( unit_test_result* parent, char const* test_c
 
 //____________________________________________________________________________//
 
-static void safe_delete( unit_test_result* ptr ) { boost::checked_delete<unit_test_result>( ptr ); }
+static void safe_delete( unit_test_result_ptr ptr ) { boost::checked_delete<unit_test_result>( ptr ); }
 
 unit_test_result::~unit_test_result()
 {
@@ -99,23 +104,22 @@ unit_test_result::~unit_test_result()
 unit_test_result&
 unit_test_result::instance()
 {
-    assert( Impl::m_head.get() != NULL );
+    assert( Impl::m_head );
 
-    return Impl::m_curr == NULL ? *Impl::m_head : *Impl::m_curr;
+    return Impl::m_curr ? *Impl::m_curr : *Impl::m_head;
 }
 
 //____________________________________________________________________________//
 
 void
-unit_test_result::test_case_start( char const* name, unit_test_counter expected_failures )
+unit_test_result::test_case_start( c_string_literal name, unit_test_counter expected_failures )
 {
-    unit_test_result* new_result_set = new unit_test_result( Impl::m_curr, name, expected_failures );
+    unit_test_result_ptr new_result_set = new unit_test_result( Impl::m_curr, name, expected_failures );
 
-    if( Impl::m_curr == NULL ) {
-        Impl::m_head.reset( new_result_set );
-    }
-    else
+    if( Impl::m_curr )
         Impl::m_curr->m_pimpl->m_children.push_back( new_result_set );
+    else
+        Impl::m_head.reset( new_result_set );
 
     Impl::m_curr = new_result_set;
 }
@@ -125,12 +129,12 @@ unit_test_result::test_case_start( char const* name, unit_test_counter expected_
 void
 unit_test_result::test_case_end()
 {
-    assert( Impl::m_curr != NULL );
+    assert( Impl::m_curr );
 
-    Impl*             curr_impl = Impl::m_curr->m_pimpl.get();
-    unit_test_result* parent    = curr_impl->m_parent;
+    Impl*                curr_impl  = Impl::m_curr->m_pimpl.get();
+    unit_test_result_ptr parent     = curr_impl->m_parent;
 
-    if( parent != NULL ) {
+    if( parent ) {
         // accumulate results
         parent->m_pimpl->m_assertions_passed += curr_impl->m_assertions_passed;
         parent->m_pimpl->m_assertions_failed += curr_impl->m_assertions_failed;
@@ -157,7 +161,7 @@ unit_test_result::increase_expected_failures( unit_test_counter amount )
 {
     m_pimpl->m_expected_failures += amount;
 
-    if( m_pimpl->m_parent != NULL )
+    if( m_pimpl->m_parent )
         m_pimpl->m_parent->increase_expected_failures( amount );
 }
 
@@ -187,7 +191,7 @@ unit_test_result::caught_exception()
 
 //____________________________________________________________________________//
 
-char const*
+c_string_literal
 unit_test_result::test_case_name()
 {
     return m_pimpl->m_test_case_name;
@@ -198,14 +202,14 @@ unit_test_result::test_case_name()
 void
 unit_test_result::reset_current_result_set()
 {
-    static unit_test_result* backup = NULL;
+    static unit_test_result_ptr backup = unit_test_result_ptr();
     static boost::scoped_ptr<unit_test_result> temporary_substitute;
 
-    assert( Impl::m_curr != NULL );
+    assert( Impl::m_curr );
 
     if( backup ) {
         Impl::m_curr = backup;
-        backup = NULL;
+        backup = unit_test_result_ptr();
         temporary_substitute.reset();
     }
     else {
@@ -227,9 +231,9 @@ unit_test_result::failures_details( unit_test_counter& num_of_failures, bool& ex
 //____________________________________________________________________________//
 
 namespace {
-std::string ps_name( bool p_s, char const* singular_form )  { return p_s ? std::string( singular_form ).append( "s" ) : std::string( singular_form ); }
-std::string cs_name( bool c_s )                             { return c_s ? "case" : "suite"; }
-std::string quote( char const* name )                       { return std::string( " \"" ).append( name ).append( "\" "); }
+std::string ps_name( bool p_s, c_string_literal singular_form )  { return p_s ? std::string( singular_form ).append( "s" ) : std::string( singular_form ); }
+std::string cs_name( bool c_s )                                  { return c_s ? "case" : "suite"; }
+std::string quote( c_string_literal name )                       { return std::string( " \"" ).append( name ).append( "\" "); }
 }
 
 //____________________________________________________________________________//
@@ -237,10 +241,9 @@ std::string quote( char const* name )                       { return std::string
 void
 unit_test_result::confirmation_report( std::ostream& where_to )
 {
-    assert( this != NULL );
+    assert( this );
 
     bool failed     = m_pimpl->m_test_cases_failed != 0 || m_pimpl->is_failed();
-    bool case_suite = m_pimpl->m_test_cases_failed + m_pimpl->m_test_cases_passed <= 1;
 
     if( failed ) {
         if( m_pimpl->m_assertions_failed > 0 ) {
@@ -252,12 +255,12 @@ unit_test_result::confirmation_report( std::ostream& where_to )
                      << ps_name( m_pimpl->m_expected_failures != 1, " failure" ) << " expected) ";
             }
 
-            where_to << " in test " << cs_name( case_suite ) 
+            where_to << " in test " << cs_name( m_pimpl->m_children.empty() ) 
                      << quote( m_pimpl->m_test_case_name );
         }
         if( m_pimpl->m_exception_caught )
-            where_to << "\n*** Test " << cs_name( case_suite )
-                     << quote( m_pimpl->m_test_case_name ) << "is aborted due to uncaught exception or fatal error";
+            where_to << "\n*** Test " << cs_name( m_pimpl->m_children.empty() )
+                     << quote( m_pimpl->m_test_case_name ) << " was aborted due to uncaught exception, user assert or system error";
         where_to << std::endl;
     }
     else
@@ -267,9 +270,9 @@ unit_test_result::confirmation_report( std::ostream& where_to )
 //____________________________________________________________________________//
 
 void
-unit_test_result::short_report( std::ostream& where_to, int indent )
+unit_test_result::short_report( std::ostream& where_to, size_t indent )
 {
-    assert( this != NULL );
+    assert( this );
 
     bool              failed           = m_pimpl->m_test_cases_failed != 0 || m_pimpl->is_failed();
     unit_test_counter total_test_cases = m_pimpl->m_test_cases_failed + m_pimpl->m_test_cases_passed;
@@ -277,7 +280,7 @@ unit_test_result::short_report( std::ostream& where_to, int indent )
     
     std::string indent_string( indent*2, ' ' );
 
-    where_to << "\n" << indent_string << "Test " << cs_name( total_test_cases <= 1 )
+    where_to << "\n" << indent_string << "Test " << cs_name( m_pimpl->m_children.empty() )
              << quote( m_pimpl->m_test_case_name )
              << (failed ? "failed with:\n" : "passed with:\n");
 
@@ -307,21 +310,25 @@ unit_test_result::short_report( std::ostream& where_to, int indent )
                  << ps_name( m_pimpl->m_expected_failures != 1, " failure" ) << " expected\n";
     }
 
+    if( m_pimpl->m_exception_caught )
+        where_to << indent_string << "Test " << cs_name( m_pimpl->m_children.empty() )
+                 << " was aborted due to uncaught exception, user assert or system error" << std::endl;
+
     where_to.flush();
 }
 
 //____________________________________________________________________________//
 
 void
-unit_test_result::detailed_report( std::ostream& where_to, int indent )
+unit_test_result::detailed_report( std::ostream& where_to, size_t indent )
 {
     short_report( where_to, indent );
 
-    std::list<unit_test_result*>::iterator it  = m_pimpl->m_children.begin();
-    std::list<unit_test_result*>::iterator end = m_pimpl->m_children.end();
+    std::list<unit_test_result_ptr>::iterator it  = m_pimpl->m_children.begin();
+    std::list<unit_test_result_ptr>::iterator end = m_pimpl->m_children.end();
 
     while( it != end ) {
-        unit_test_result* next = *(it++);
+        unit_test_result_ptr next = *(it++);
         next->detailed_report( where_to, indent+1 );
     }
 }
@@ -344,6 +351,10 @@ unit_test_result::result_code()
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.12  2002/12/08 18:16:57  rogeeff
+//  eliminated all uses of NULL
+//  switched to use c_string_literal
+//
 //  Revision 1.11  2002/11/02 20:04:42  rogeeff
 //  release 1.29.0 merged into the main trank
 //
