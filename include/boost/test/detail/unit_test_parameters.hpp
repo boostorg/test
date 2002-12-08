5cb8d4f3096e4c50bd93d1b8fdc9791d6eba2246
Gennadiy Rozental <rogeeff@gmail.com>
2002-12-08 17:38:44 +0000

    catch_system_error framework cla parameter and envronment variable introduced switch to use c_string_literal
    [SVN r16537]

--- include/boost/test/detail/unit_test_parameters.hpp
+++ include/boost/test/detail/unit_test_parameters.hpp
@@ -19,22 +19,25 @@
 // STL
 #include <string>
 
+#include <boost/test/detail/unit_test_config.hpp>
+
 namespace boost {
 
 namespace unit_test_framework {
 
 // framework parameters and there corresponding command-line arguments
-char const* const LOGLEVEL          = "BOOST_TEST_LOG_LEVEL";        // --log_level
-char const* const NO_RESULT_CODE    = "BOOST_TEST_RESULT_CODE";      // --result_code
-char const* const RESULT_REPORT     = "BOOST_TEST_REPORT_LEVEL";     // --result_report
-char const* const TESTS_TO_RUN      = "BOOST_TESTS_TO_RUN";          // --run_test
-char const* const SAVE_TEST_PATTERN = "BOOST_TEST_SAVE_PATTERN";     // --save_pattern
-char const* const BUILD_INFO        = "BOOST_TEST_BUILD_INFO";       // --build_info
+c_string_literal const LOGLEVEL          = "BOOST_TEST_LOG_LEVEL";              // --log_level
+c_string_literal const NO_RESULT_CODE    = "BOOST_TEST_RESULT_CODE";            // --result_code
+c_string_literal const RESULT_REPORT     = "BOOST_TEST_REPORT_LEVEL";           // --result_report
+c_string_literal const TESTS_TO_RUN      = "BOOST_TESTS_TO_RUN";                // --run_test
+c_string_literal const SAVE_TEST_PATTERN = "BOOST_TEST_SAVE_PATTERN";           // --save_pattern
+c_string_literal const BUILD_INFO        = "BOOST_TEST_BUILD_INFO";             // --build_info
+c_string_literal const CATCH_SYS_ERRORS  = "BOOST_TEST_CATCH_SYSTEM_ERRORS";    // --catch_system_errors
 
-enum result_report_level                { CONFIRMATION_REPORT, SHORT_REPORT, DETAILED_REPORT, NO_REPORT };
-char const* const report_level_names[] = { "confirm"          , "short"     , "detailed"     , "no"      };
+enum result_report_level                      { CONFIRMATION_REPORT, SHORT_REPORT, DETAILED_REPORT, NO_REPORT };
+c_string_literal const report_level_names[] = { "confirm"          , "short"     , "detailed"     , "no"      };
 
-std::string retrieve_framework_parameter( char const* parameter_name_, int* argc_ = NULL, char ** argv_ = NULL );
+std::string retrieve_framework_parameter( c_string_literal parameter_name_, int* argc_, char** argv_ );
 
 } // namespace unit_test_framework
 
@@ -44,6 +47,10 @@ std::string retrieve_framework_parameter( char const* parameter_name_, int* argc
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.6  2002/12/08 17:38:44  rogeeff
+//  catch_system_error framework cla parameter and envronment variable introduced
+//  switch to use c_string_literal
+//
 //  Revision 1.5  2002/11/02 19:31:05  rogeeff
 //  merged into the main trank
 //
