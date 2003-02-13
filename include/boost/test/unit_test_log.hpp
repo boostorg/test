7093b054afd1bccfb61bd88eca9e04f21029c62e
Gennadiy Rozental <rogeeff@gmail.com>
2003-02-13 08:20:56 +0000

    report_level->log_level log format config methods added log interface slightly changed to allow multiple log formats Unused macros deleted
    [SVN r17346]

--- include/boost/test/unit_test_log.hpp
+++ include/boost/test/unit_test_log.hpp
@@ -20,6 +20,7 @@
 
 // Boost.Test
 #include <boost/test/detail/unit_test_config.hpp>
+#include <boost/test/detail/unit_test_parameters.hpp>
 
 // BOOST
 #include <boost/utility.hpp>
@@ -32,19 +33,19 @@ namespace boost {
 
 namespace unit_test_framework {
 
-//  each reporting level includes all subsequent higher reporting levels
-enum            report_level {
-    report_successful_tests     = 0,
-    report_test_suites          = 1,
-    report_messages             = 2,
-    report_warnings             = 3,
-    report_all_errors           = 4, // reported by unit test macros
-    report_cpp_exception_errors = 5, // uncaught C++ exceptions
-    report_system_errors        = 6, // including timeouts, signals, traps
-    report_fatal_errors         = 7, // including unit test macros or
+//  each log level includes all subsequent higher loging levels
+enum            log_level {
+    log_successful_tests     = 0,
+    log_test_suites          = 1,
+    log_messages             = 2,
+    log_warnings             = 3,
+    log_all_errors           = 4, // reported by unit test macros
+    log_cpp_exception_errors = 5, // uncaught C++ exceptions
+    log_system_errors        = 6, // including timeouts, signals, traps
+    log_fatal_errors         = 7, // including unit test macros or
                                      // fatal system errors
-    report_progress_only        = 8, // only unit test progress to be reported
-    report_nothing              = 9
+    log_progress_only        = 8, // only unit test progress to be reported
+    log_nothing              = 9
 };
 
 // ************************************************************************** //
@@ -58,15 +59,15 @@ struct end {
 };
 
 struct level {
-    explicit    level( report_level l_ ) : m_level( l_ ) {}
+    explicit    level( log_level l_ ) : m_level( l_ ) {}
 
-    report_level m_level;
+    log_level m_level;
 };
 
 struct line {
-    explicit    line( unsigned int ln_ ) : m_line_num( ln_ ) {}
+    explicit    line( std::size_t ln_ ) : m_line_num( ln_ ) {}
 
-    unsigned int m_line_num;
+    std::size_t m_line_num;
 };
 
 struct file {
@@ -81,18 +82,20 @@ struct checkpoint {
     std::string const& m_message;
 };
 
-struct report_exception {
-    report_exception( c_string_literal what_ ) : m_what( what_ ) {}
+struct log_exception {
+    explicit    log_exception( c_string_literal what_ ) : m_what( what_ ) {}
 
     c_string_literal     m_what;
 };
 
-struct report_progress {
+struct log_progress {
 };
 
 // ************************************************************************** //
 // **************                 unit_test_log                ************** //
 // ************************************************************************** //
+
+class test_case;
 
 class unit_test_log : private boost::noncopyable { //!! Singleton
 public:
@@ -102,12 +105,20 @@ public:
     // instance access method;
     static unit_test_log& instance();
 
+
+    void            start( bool print_build_info_ = false );
+    void            header( unit_test_counter test_cases_amount_ );
+    void            finish( unit_test_counter test_cases_amount_ );
+
     // log configuration methods
-    void            start( unit_test_counter test_cases_amount_, bool print_build_info_ = false );
     void            set_log_stream( std::ostream& str_ );
-    void            set_log_threshold_level( report_level lev_ );
-    void            set_log_threshold_level_by_name( c_string_literal lev_ );
+    void            set_log_threshold_level( log_level lev_ );
+    void            set_log_threshold_level_by_name( std::string const& lev_ );
+    void            set_log_format( std::string const& of );
     void            clear_checkpoint();
+
+    // test case scope tracking
+    void            track_test_case_scope( test_case const& tc, bool in_out );
 
     // entry configuration methods
     unit_test_log&  operator<<( begin const& );         // begin entry 
@@ -118,36 +129,29 @@ public:
     unit_test_log&  operator<<( checkpoint const& );    // set checkpoint
 
     // print value_ methods
-    unit_test_log&  operator<<( report_progress const& );
-    unit_test_log&  operator<<( report_exception const& );
+    unit_test_log&  operator<<( log_progress const& );
+    unit_test_log&  operator<<( log_exception const& );
     unit_test_log&  operator<<( c_string_literal value_ );
     unit_test_log&  operator<<( std::string const& value_ );
 
 private:
     // Constructor
     unit_test_log();
+    friend class unit_test_log_formatter;
 
     struct          Impl;
     Impl*           m_pimpl;
 }; // unit_test_log
 
-// helper macro
+// helper macros
 #define BOOST_UT_LOG_BEGIN( file_name, line_num, loglevel )                             \
     boost::unit_test_framework::unit_test_log::instance()                               \
                                      << boost::unit_test_framework::begin()             \
                                      << boost::unit_test_framework::level( loglevel )   \
                                      << boost::unit_test_framework::file( file_name )   \
-                                     << boost::unit_test_framework::line( line_num ) <<
-
-#define BOOST_UT_LOCAL_LOG_BEGIN( loglevel ) BOOST_UT_LOG_BEGIN( __FILE__, __LINE__, boost::unit_test_framework::loglevel )
-#define BOOST_UT_MESSAGE_BEGIN( loglevel )                                              \
-    boost::unit_test_framework::unit_test_log::instance()                               \
-                                     << boost::unit_test_framework::begin()             \
-                                     << boost::unit_test_framework::level( boost::unit_test_framework::loglevel ) <<
-
-#define BOOST_TEST_SUITE_MESSAGE_BEGIN BOOST_UT_MESSAGE_BEGIN( report_test_suites )
+                                     << boost::unit_test_framework::line( line_num ) << \
+/**/
 #define BOOST_UT_LOG_END             << boost::unit_test_framework::end();
-#define BOOST_TEST_SUITE_MESSAGE_END << boost::unit_test_framework::end();
 
 } // namespace unit_test_framework
 
@@ -157,6 +161,12 @@ private:
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.14  2003/02/13 08:20:56  rogeeff
+//  report_level->log_level
+//  log format config methods added
+//  log interface slightly changed to allow multiple log formats
+//  Unused macros deleted
+//
 //  Revision 1.13  2002/12/08 17:43:55  rogeeff
 //  switched to use c_string_literal
 //
