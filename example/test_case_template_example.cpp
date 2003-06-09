2e78750f0c056d5c8061566d56700ef0f2580c6b
Gennadiy Rozental <rogeeff@gmail.com>
2003-06-09 09:26:18 +0000

    BOOST_FUNC_TEMPLATE_TEST_CASE example
    [SVN r18731]

---
+++ example/test_case_template_example.cpp
@@ -0,0 +1,35 @@
+//  (C) Copyright Gennadiy Rozental 2003.
+//  Permission to copy, use, modify, sell and distribute this software
+//  is granted provided this copyright notice appears in all copies.
+//  This software is provided "as is" without express or implied warranty,
+//  and with no claim as to its suitability for any purpose.
+//
+//  See http://www.boost.org for most recent version including documentation.
+
+// Boost.Test
+#include <boost/test/test_tools.hpp>
+#include <boost/test/test_case_template.hpp>
+using boost::unit_test_framework::test_suite;
+
+// Boost.MPL
+#include "boost/mpl/range_c.hpp"
+
+template<typename Number>
+void free_test_function( Number* = 0 ) {
+    BOOST_CHECK_EQUAL( 2, (int const)Number::value );
+}
+
+BOOST_META_FUNC_TEST_CASE( free_test_function );
+
+test_suite*
+init_unit_test_suite( int, char* [] ) {
+    test_suite* test= BOOST_TEST_SUITE( "Test case template example" );
+
+    typedef boost::mpl::range_c<int,0,10> numbers;
+
+    test->add( BOOST_FUNC_TEMPLATE_TEST_CASE( free_test_function, numbers ) );
+
+    return test;
+}
+
+// EOF
