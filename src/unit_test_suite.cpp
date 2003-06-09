71803d4b46d180092ced3a301c3f2965b238cc7d
Gennadiy Rozental <rogeeff@gmail.com>
2003-06-09 09:21:04 +0000

    dependency check support simplified testing abort procedures
    [SVN r18727]

--- src/unit_test_suite.cpp
+++ src/unit_test_suite.cpp
@@ -1,4 +1,4 @@
-//  (C) Copyright Gennadiy Rozental 2001-2002.
+//  (C) Copyright Gennadiy Rozental 2001-2003.
 //  Permission to copy, use, modify, sell and distribute this software
 //  is granted provided this copyright notice appears in all copies.
 //  This software is provided "as is" without express or implied warranty,
@@ -8,7 +8,7 @@
 //
 //  File        : $RCSfile$
 //
-//  Version     : $Id$
+//  Version     : $Revision$
 //
 //  Description : privide core implementation for Unit Test Framework. 
 //  Extensions could be provided in separate files
@@ -18,6 +18,9 @@
 #include <boost/test/unit_test_suite.hpp>
 #include <boost/test/unit_test_log.hpp>
 #include <boost/test/unit_test_result.hpp>
+
+// BOOST
+#include <boost/mem_fn.hpp>
 
 // STL
 #include <list>
@@ -31,10 +34,40 @@ namespace unit_test_framework {
 // **************                   test_case                  ************** //
 // ************************************************************************** //
 
-test_case::test_case( std::string const& name_, bool type, unit_test_counter stages_number_, bool monitor_run_ )
+typedef unit_test_result const* unit_test_result_cptr;
+
+struct test_case::Impl {
+    Impl( bool monitor_run_ ) : m_monitor_run( monitor_run_ ), m_results_set( unit_test_result_cptr() ) {}
+
+    bool                        m_monitor_run;          // true - unit_test_monitor will be user to monitor running
+                                                        // of implementation function
+    std::list<test_case const*> m_dependencies_list;    // list of test cases this test case depends on. We won't run it until they pass
+    unit_test_result_cptr       m_results_set;          // results set instance reference for this test case
+    
+    static bool                 s_abort_testing;        // used to flag critical error and try gracefully stop testing
+
+    bool                        check_dependencies();
+};
+
+bool test_case::Impl::s_abort_testing = false;
+
+//____________________________________________________________________________//
+
+
+inline bool
+test_case::Impl::check_dependencies()
+{
+    return std::find_if( m_dependencies_list.begin(), 
+                         m_dependencies_list.end(), 
+                         std::not1( boost::mem_fn( &test_case::has_passed ) ) ) == m_dependencies_list.end();
+}
+
+//____________________________________________________________________________//
+
+test_case::test_case( std::string const& name_, bool type, unit_test_counter stages_amount_, bool monitor_run_ )
 : p_timeout( 0 ), p_expected_failures( 0 ), p_type( type ),
-  p_name( name_ ), p_compound_stage( false ), p_stages_amount( stages_number_ ),
-  m_monitor_run( monitor_run_ )
+  p_name( name_ ), p_compound_stage( false ), p_stages_amount( stages_amount_ ),
+  m_pimpl( new Impl( monitor_run_ ) )
 {
 }
 
@@ -49,38 +82,77 @@ test_case::size() const
 //____________________________________________________________________________//
 
 void
+test_case::depends_on( test_case const* rhs )
+{
+    m_pimpl->m_dependencies_list.push_back( rhs );
+}
+
+//____________________________________________________________________________//
+
+bool
+test_case::has_passed() const
+{
+    return m_pimpl->m_results_set != unit_test_result_cptr() && m_pimpl->m_results_set->has_passed();
+}
+
+//____________________________________________________________________________//
+
+void
 test_case::run()
 {
-    unit_test_log::instance().track_test_case_scope( *this, true );
+    using detail::unit_test_monitor;
+
+    test_case_scope_tracker scope_tracker( *this );
     
-    bool is_initialized  = true;
-         s_abort_testing = false;
+    // 0. Check if we allowed to run this test case
+    if( !m_pimpl->check_dependencies() )
+        return;
+
+    m_pimpl->s_abort_testing = false;
 
     // 1. Init test results
-    unit_test_result::test_case_start( p_name, p_expected_failures );
+    unit_test_result_tracker result_tracker( p_name, p_expected_failures );
+    m_pimpl->m_results_set = &unit_test_result::instance();
 
     // 2. Initialize test case
-    if( m_monitor_run ) {
+    if( m_pimpl->m_monitor_run ) {
         error_level_type init_result =
-            detail::unit_test_monitor( *this, &test_case::do_init ).execute_and_translate( p_timeout );
+            unit_test_monitor( *this, &test_case::do_init ).execute_and_translate( p_timeout );
 
-        is_initialized   = init_result == detail::unit_test_monitor::test_ok;
-        s_abort_testing  = detail::unit_test_monitor::is_critical_error( init_result );
+        if( init_result != unit_test_monitor::test_ok ) {
+            m_pimpl->s_abort_testing  = unit_test_monitor::is_critical_error( init_result );
+
+            BOOST_UT_LOG_BEGIN( __FILE__, __LINE__, log_fatal_errors )
+                "Test case initialization has failed"
+            BOOST_UT_LOG_END;
+
+            return;
+        }
     }
     else {
         do_init();
     }
 
     // 3. Run test case (all stages)
-    for( unit_test_counter i=0; i != p_stages_amount && !s_abort_testing && is_initialized; ++i ) {
+    for( unit_test_counter i=0; i != p_stages_amount; ++i ) {
         p_compound_stage.value = false; // could be set by do_run to mark compound stage;
                                         // than no need to report progress here
 
-        if( m_monitor_run ) {
+        if( m_pimpl->m_monitor_run ) {
             error_level_type run_result =
-                detail::unit_test_monitor( *this, &test_case::do_run ).execute_and_translate( p_timeout );
+                unit_test_monitor( *this, &test_case::do_run ).execute_and_translate( p_timeout );
 
-            s_abort_testing = detail::unit_test_monitor::is_critical_error( run_result );
+            if( unit_test_monitor::is_critical_error( run_result ) ) {
+                m_pimpl->s_abort_testing = true;
+
+                BOOST_UT_LOG_BEGIN( __FILE__, __LINE__, log_fatal_errors )
+                    "Testing aborted"
+                BOOST_UT_LOG_END;
+            }
+
+            if( m_pimpl->s_abort_testing )
+                return;
+
         }
         else {
             do_run();
@@ -91,31 +163,18 @@ test_case::run()
     }
 
     // 3. Finalize test case
-    if( is_initialized && !s_abort_testing ) {
-        if( m_monitor_run ) {
-            error_level_type destroy_result =
-                detail::unit_test_monitor( *this, &test_case::do_destroy ).execute_and_translate( p_timeout );
-
-            s_abort_testing = detail::unit_test_monitor::is_critical_error( destroy_result );
-        }
-        else {
-            do_destroy();
-        }
+    if( m_pimpl->m_monitor_run ) {
+        error_level_type destroy_result =
+            unit_test_monitor( *this, &test_case::do_destroy ).execute_and_translate( p_timeout );
+        
+        m_pimpl->s_abort_testing = unit_test_monitor::is_critical_error( destroy_result );
     }
-
-    if( s_abort_testing ) {
-        BOOST_UT_LOG_BEGIN( __FILE__, __LINE__, log_fatal_errors )
-            "testing aborted"
-        BOOST_UT_LOG_END
+    else {
+        do_destroy();
     }
-
-    unit_test_result::test_case_end();
-    unit_test_log::instance().track_test_case_scope( *this, false );
 }
 
 //____________________________________________________________________________//
-
-bool test_case::s_abort_testing = false;
 
 // ************************************************************************** //
 // **************                  test_suite                  ************** //
@@ -218,15 +277,9 @@ normalize_test_case_name( std::string& name_ )
 //  Revision History :
 //  
 //  $Log$
-//  Revision 1.7  2003/02/13 08:39:05  rogeeff
-//  type: virtual function -> property
-//  C strings eliminated
-//
-//  Revision 1.6  2002/12/08 18:15:19  rogeeff
-//  switched to use c_string_literal
-//
-//  Revision 1.5  2002/11/02 20:04:42  rogeeff
-//  release 1.29.0 merged into the main trank
+//  Revision 1.8  2003/06/09 09:21:04  rogeeff
+//  dependency check support
+//  simplified testing abort procedures
 //
 
 // ***************************************************************************
