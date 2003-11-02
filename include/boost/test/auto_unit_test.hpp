315c32016edd2c0e8e773674ba082521ffaa9db7
Gennadiy Rozental <rogeeff@gmail.com>
2003-11-02 06:16:14 +0000

    fixed to support multi-module unit testing
    [SVN r20594]

--- include/boost/test/auto_unit_test.hpp
+++ include/boost/test/auto_unit_test.hpp
@@ -17,26 +17,29 @@
 // Boost.Test
 #include <boost/test/unit_test.hpp>
 
-static boost::unit_test_framework::test_suite* test = BOOST_TEST_SUITE( "Auto Unit Test" );
-
-boost::unit_test_framework::test_suite*
-init_unit_test_suite( int /* argc */, char* /* argv */ [] ) {
-    return test;
-}
-
 // ************************************************************************** //
 // **************           auto_unit_test_registrar           ************** //
 // ************************************************************************** //
 
 namespace boost {
-
 namespace unit_test_framework {
+namespace detail {
+
+inline boost::unit_test_framework::test_suite*
+auto_unit_test_suite()
+{
+	static boost::unit_test_framework::test_suite* inst = BOOST_TEST_SUITE( "Auto Unit Test" );
+
+	return inst;
+}
 
 struct auto_unit_test_registrar
 {
     // Constructor
-    explicit auto_unit_test_registrar( test_case* tc ) { test->add( tc ); }
+    explicit auto_unit_test_registrar( test_case* tc ) { auto_unit_test_suite()->add( tc ); }
 };
+
+} // detail
 
 } // unit_test_framework
 
@@ -48,16 +51,30 @@ struct auto_unit_test_registrar
 
 #define BOOST_AUTO_UNIT_TEST( func_name )                           \
 static void func_name();                                            \
-static boost::unit_test_framework::auto_unit_test_registrar         \
+static boost::unit_test_framework::detail::auto_unit_test_registrar \
     BOOST_JOIN( test_registrar, __LINE__)                           \
         ( BOOST_TEST_CASE( func_name ) );                           \
 static void func_name()                                             \
 /**/
 
+// ************************************************************************** //
+// **************             BOOST_AUTO_UNIT_TEST             ************** //
+// ************************************************************************** //
+
+#ifdef BOOST_AUTO_TEST_MAIN
+boost::unit_test_framework::test_suite*
+init_unit_test_suite( int /* argc */, char* /* argv */ [] ) {
+	return boost::unit_test_framework::detail::auto_unit_test_suite();
+}
+#endif
+
 // ***************************************************************************
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.5  2003/11/02 06:16:14  rogeeff
+//  fixed to support multi-module unit testing
+//
 //  Revision 1.4  2003/10/27 07:13:12  rogeeff
 //  licence update
 //
