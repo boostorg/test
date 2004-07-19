b9932e5674883f1e7299de471c27ddf603b414a3
Gennadiy Rozental <rogeeff@gmail.com>
2004-07-19 12:09:25 +0000

    added proper encoded of XML PCDATA
    [SVN r23783]

--- src/supplied_log_formatters.cpp
+++ src/supplied_log_formatters.cpp
@@ -60,7 +60,7 @@ void
 msvc65_like_log_formatter::log_header( std::ostream& output, unit_test_counter test_cases_amount )
 {
     output  << "Running " << test_cases_amount << " test "
-        << (test_cases_amount > 1 ? "cases" : "case") << "...\n";
+            << (test_cases_amount > 1 ? "cases" : "case") << "...\n";
 }
 
 //____________________________________________________________________________//
@@ -75,9 +75,9 @@ msvc65_like_log_formatter::finish_log( std::ostream& /* output */ )
 void
 msvc65_like_log_formatter::track_test_case_scope( std::ostream& output, test_case const& tc, bool in_out )
 {
-    output << (in_out ? "Entering" : "Leaving")
-        << " test " << ( tc.p_type ? "case" : "suite" )
-        << " \"" << tc.p_name << "\"";
+    output  << (in_out ? "Entering" : "Leaving")
+            << " test " << ( tc.p_type ? "case" : "suite" )
+            << " \"" << tc.p_name << "\"";
 }
 
 //____________________________________________________________________________//
@@ -164,13 +164,14 @@ xml_log_formatter::start_log( std::ostream& output, bool log_build_info )
 {
     output  << "<TestLog";
 
-    if( log_build_info )
-        output  << " platform=\"" << BOOST_PLATFORM            << '\"'
-                << " compiler=\"" << BOOST_COMPILER            << '\"'
-                << " stl=\""      << BOOST_STDLIB              << '\"'
-                << " boost=\""    << BOOST_VERSION/100000      << "."
-                                  << BOOST_VERSION/100 % 1000  << "."
-                                  << BOOST_VERSION % 100       << '\"';
+    if( log_build_info ) {
+        output  << " platform"; print_attr_value( output, BOOST_PLATFORM )
+                << " compiler"; print_attr_value( output, BOOST_COMPILER )
+                << " stl";      print_attr_value( output, BOOST_STDLIB )
+                << " boost=\""  << BOOST_VERSION/100000     << "."
+                                << BOOST_VERSION/100 % 1000 << "."
+                                << BOOST_VERSION % 100      << '\"';
+    }
 
     output  << ">\n";
 }
@@ -200,11 +201,10 @@ xml_log_formatter::track_test_case_scope( std::ostream& output, test_case const&
 
     print_indent( output );
 
-    output << (in_out ? "<" : "</")
-        << ( tc.p_type ? "TestCase" : "TestSuite" );
+    output << (in_out ? "<" : "</") << ( tc.p_type ? "TestCase" : "TestSuite" );
 
     if( in_out )
-        output << " name=\"" << tc.p_name << "\"";
+        output << " name";  print_attr_value( output, tc.p_name );
 
     output << ">";
 
@@ -218,24 +218,25 @@ void
 xml_log_formatter::log_exception( std::ostream& output, const_string test_case_name, const_string explanation )
 {
     print_indent( output );
-    output << "<Exception name=\"" << test_case_name << "\">\n";
+    output << "<Exception name"; print_attr_value( output, test_case_name ) << ">\n";
     
     m_indent += 2;
 
     print_indent( output );
-    output << explanation << '\n';
+    print_pcdata( output, explanation ) << '\n';
 
     log_checkpoint_data const& chpd = checkpoint_data();
 
     if( !chpd.m_message.empty() ) {
         print_indent( output );
-        output << "<LastCheckpoint file=\"" << chpd.m_file << "\""
-            << " line=\"" << chpd.m_line << "\">\n";
+        output << "<LastCheckpoint file";   print_attr_value( output, chpd.m_file )
+               << " line";                  print_attr_value( output, chpd.m_line ) 
+               << ">\n";
 
         m_indent += 2;
 
         print_indent( output );
-        output << chpd.m_message << "\n";
+        print_pcdata( output, chpd.m_message ) << "\n";
 
         m_indent -= 2;
 
@@ -260,9 +261,9 @@ xml_log_formatter::begin_log_entry( std::ostream& output, log_entry_types let )
 
     m_curr_tag = xml_tags[let];
     output << '<' << m_curr_tag
-        << " file=\"" << entry_data().m_file<< '\"'
-        << " line=\"" << entry_data().m_line << '\"'
-        << ">\n";
+           << " file";  print_attr_value( output, entry_data().m_file )
+           << " line";  print_attr_value( output, entry_data().m_line )
+           << ">\n";
 
     m_indent += 2;
     print_indent( output );
@@ -273,7 +274,7 @@ xml_log_formatter::begin_log_entry( std::ostream& output, log_entry_types let )
 void
 xml_log_formatter::log_entry_value( std::ostream& output, const_string value )
 {
-    output << value;
+    print_pcdata( output, value );
 }
 
 //____________________________________________________________________________//
@@ -314,6 +315,9 @@ xml_log_formatter::print_indent( std::ostream& output )
 //  Revision History :
 //
 //  $Log$
+//  Revision 1.11  2004/07/19 12:09:25  rogeeff
+//  added proper encoded of XML PCDATA
+//
 //  Revision 1.10  2004/06/07 07:34:22  rogeeff
 //  detail namespace renamed
 //
