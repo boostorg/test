494380972d0e3363d1273d90e866c46a341eb8a1
Gennadiy Rozental <rogeeff@gmail.com>
2004-09-17 12:31:30 +0000

    *** empty log message ***
    [SVN r25172]

--- doc/components/unit_test_framework/getting_started/const_string.hpp
+++ doc/components/unit_test_framework/getting_started/const_string.hpp
@@ -1,8 +1,16 @@
-//  (C) Copyright Gennadiy Rozental 2001.
-//  Permission to copy, use, modify, sell and distribute this software
-//  is granted provided this copyright notice appears in all copies.
-//  This software is provided "as is" without express or implied warranty,
-//  and with no claim as to its suitability for any purpose.
+//  (C) Copyright Gennadiy Rozental 2001-2004.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+//  File        : $RCSfile$
+//
+//  Version     : $Revision$
+//
+//  Description : simple string class definition
+// ***************************************************************************
 
 #ifndef  CONST_STRING_HPP
 #define  CONST_STRING_HPP
