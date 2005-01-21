0fb32260fe2f1db9fd8a9f3f1333e01b01be6b83
Gennadiy Rozental <rogeeff@gmail.com>
2005-01-21 07:31:44 +0000

    xml helper facilities reworked to present manipulator interfaces
    [SVN r26774]

--- include/boost/test/detail/xml_printer.hpp
+++ include/boost/test/detail/xml_printer.hpp
@@ -18,6 +18,7 @@
 // Boost.Test
 #include <boost/test/detail/basic_cstring/basic_cstring.hpp>
 #include <boost/test/detail/fixed_mapping.hpp>
+#include <boost/test/detail/custom_manip.hpp>
 
 // BOOST
 #include <boost/config.hpp>
@@ -25,69 +26,97 @@
 // STL
 #include <iostream>
 
+#include <boost/test/detail/suppress_warnings.hpp>
+
 namespace boost {
 
 namespace unit_test {
 
+#if !defined(BOOST_NO_ARGUMENT_DEPENDENT_LOOKUP) && !BOOST_WORKAROUND(__BORLANDC__, < 0x564)
 namespace ut_detail {
+#endif
 
 // ************************************************************************** //
-// **************                  xml_printer                 ************** //
+// **************               xml print helpers              ************** //
 // ************************************************************************** //
 
-class xml_printer {
-    static inline std::ostream& print_escaped( std::ostream& where_to, const_string value )
-    {
-        static fixed_mapping<char,char const*> char_type(
-            '<' , "lt",
-            '>' , "gt",
-            '&' , "amp",
-            '\'', "apos" ,
-            '"' , "quot",
+inline void
+print_escaped( std::ostream& where_to, const_string value )
+{
+    static fixed_mapping<char,char const*> char_type(
+        '<' , "lt",
+        '>' , "gt",
+        '&' , "amp",
+        '\'', "apos" ,
+        '"' , "quot",
 
-            0
-            );
+        0
+    );
 
-        for( const_string::iterator it = value.begin(); it != value.end(); ++it ) {
-            char const* ref = char_type[*it];
+    for( const_string::iterator it = value.begin(); it != value.end(); ++it ) {
+        char const* ref = char_type[*it];
 
-            if( ref )
-                where_to << '&' << ref << ';';
-            else
-                where_to << *it;
-        }
-
-        return where_to;
+        if( ref )
+            where_to << '&' << ref << ';';
+        else
+            where_to << *it;
     }
-protected:
-    static inline std::ostream&    print_attr_value( std::ostream& where_to, const_string value )
-    {
-        where_to << "=\"";
-        return print_escaped( where_to, value ) << '"';
-    }
+}
 
-    template<typename T>
-    static inline std::ostream&    print_attr_value( std::ostream& where_to, T const& value )
-    {
-        return where_to << "=\"" << value << '"';
-    }
+//____________________________________________________________________________//
 
-    static inline std::ostream&    print_pcdata( std::ostream& where_to, const_string value )
-    {
-        return print_escaped( where_to, value );
-    }
-};
+typedef custom_manip<struct attr_value_t> attr_value;
 
+inline std::ostream&
+operator<<( custom_printer<attr_value> const& p, const_string value )
+{
+    *p << "=\"";
+    print_escaped( *p, value );
+    *p << '"';
+
+    return *p;
+}
+
+//____________________________________________________________________________//
+
+template<typename T>
+inline std::ostream&
+operator<<( custom_printer<attr_value> const& p, T const& value )
+{
+    return *p<< "=\"" << value << '"';
+}
+
+//____________________________________________________________________________//
+
+typedef custom_manip<struct pcdata_t> pcdata;
+
+inline std::ostream&
+operator<<( custom_printer<pcdata> const& p, const_string value )
+{
+    print_escaped( *p, value );
+
+    return *p;
+}
+
+//____________________________________________________________________________//
+
+#if !defined(BOOST_NO_ARGUMENT_DEPENDENT_LOOKUP) && !BOOST_WORKAROUND(__BORLANDC__, < 0x564)
 } // namespace ut_detail
+#endif
 
 } // namespace unit_test
 
 } // namespace boost
 
+#include <boost/test/detail/enable_warnings.hpp>
+
 // ***************************************************************************
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.3  2005/01/21 07:31:44  rogeeff
+//  xml helper facilities reworked to present manipulator interfaces
+//
 //  Revision 1.2  2004/08/04 02:50:27  rogeeff
 //  darwin workarounds
 //
