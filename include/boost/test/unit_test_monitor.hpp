5b76fd22a24a723f791f6931febc59ac354bd79e
Gennadiy Rozental <rogeeff@gmail.com>
2005-12-14 05:55:35 +0000

    dll support introduced
    [SVN r32046]

--- include/boost/test/unit_test_monitor.hpp
+++ include/boost/test/unit_test_monitor.hpp
@@ -34,7 +34,7 @@ namespace unit_test {
 // **************               unit_test_monitor              ************** //
 // ************************************************************************** //
 
-class unit_test_monitor_t : public singleton<unit_test_monitor_t>, public execution_monitor {
+class BOOST_TEST_DECL unit_test_monitor_t : public singleton<unit_test_monitor_t>, public execution_monitor {
 public:
     enum error_level { 
         test_fail               =  1,
@@ -70,6 +70,9 @@ BOOST_TEST_SINGLETON_INST( unit_test_monitor )
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.2  2005/12/14 05:55:35  rogeeff
+//  dll support introduced
+//
 //  Revision 1.1  2005/02/20 08:27:06  rogeeff
 //  This a major update for Boost.Test framework. See release docs for complete list of fixes/updates
 //
