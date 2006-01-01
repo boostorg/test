019bd8f5efccc3ee0115c04ed8227593d4f2a830
Douglas Gregor <doug.gregor@gmail.com>
2006-01-01 17:29:38 +0000

    Work around anonymous namespace bug in Apple GCC 3.3
    [SVN r32198]

--- include/boost/test/utils/trivial_singleton.hpp
+++ include/boost/test/utils/trivial_singleton.hpp
@@ -51,6 +51,10 @@ type() {}                                       \
 template class unit_test::singleton< BOOST_JOIN( inst, _t ) > ; \
 namespace { BOOST_JOIN( inst, _t)& inst = BOOST_JOIN( inst, _t)::instance(); }
 
+#elif defined(__APPLE_CC__) && defined(__GNUC__) && __GNUC__ < 4
+#define BOOST_TEST_SINGLETON_INST( inst ) \
+static BOOST_JOIN( inst, _t)& inst = BOOST_JOIN (inst, _t)::instance();
+
 #else
 
 #define BOOST_TEST_SINGLETON_INST( inst ) \
@@ -68,6 +72,9 @@ namespace { BOOST_JOIN( inst, _t)& inst = BOOST_JOIN( inst, _t)::instance(); }
 //  Revision History :
 //  
 //  $Log$
+//  Revision 1.3  2006/01/01 17:29:38  dgregor
+//  Work around anonymous namespace bug in Apple GCC 3.3
+//
 //  Revision 1.2  2005/06/15 07:21:51  schoepflin
 //  Tru64 needs an explicit instantiation of the singleton template. Otherwise we
 //  end up with multiple singleton instances.
