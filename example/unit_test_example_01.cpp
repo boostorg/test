0aff4022152f479437a2a3f74a304212100a6168
Gennadiy Rozental <rogeeff@gmail.com>
2009-12-13 04:21:43 +0000

    revert wrong changes
    [SVN r58335]

--- example/unit_test_example_01.cpp
+++ example/unit_test_example_01.cpp
@@ -12,8 +12,28 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
+//____________________________________________________________________________//
+
+// most frequently you implement test cases as a free functions with automatic registration
 BOOST_AUTO_TEST_CASE( test1 )
 {
+    // reports 'error in "test1": test 2 == 1 failed'
+    BOOST_CHECK( 2 == 1 );
 }
+
+//____________________________________________________________________________//
+
+// each test file may contain any number of test cases; each test case has to have unique name
+BOOST_AUTO_TEST_CASE( test2 )
+{
+    int i = 0;
+
+    // reports 'error in "test2": check i == 2 failed [0 != 2]'
+    BOOST_CHECK_EQUAL( i, 2 );
+
+    BOOST_CHECK_EQUAL( i, 0 );
+}
+
+//____________________________________________________________________________//
 
 // EOF
