22e8866efb9895129eb36dc4efc23766df5ba5f2
Gennadiy Rozental <rogeeff@gmail.com>
2010-05-16 03:58:23 +0000

    examples update
    [SVN r62017]

--- example/unit_test_example_11.cpp
+++ example/unit_test_example_11.cpp
@@ -1,4 +1,4 @@
-//  (C) Copyright Gennadiy Rozental 2002-2008.
+//  (C) Copyright Gennadiy Rozental 2002-2010.
 //  (C) Copyright Gennadiy Rozental & Ullrich Koethe 2001.
 //  Distributed under the Boost Software License, Version 1.0.
 //  (See accompanying file LICENSE_1_0.txt or copy at 
@@ -15,6 +15,9 @@ using namespace boost::unit_test;
 #include <vector>
 #include <string>
 
+#define LOG_FATAL_ERROR( M )                                  \
+BOOST_TEST_LOG_ENTRY( ::boost::unit_test::log_fatal_errors )  \
+    << (::boost::unit_test::lazy_ostream::instance() << M)
 //____________________________________________________________________________//
 
 // this free function is invoked with all parameters specified in a list
@@ -29,6 +32,8 @@ void check_string( std::string const& s )
 test_suite*
 init_unit_test_suite( int /*argc*/, char* /*argv*/[] ) {
     framework::master_test_suite().p_name.value = "Unit test example 11";
+
+    LOG_FATAL_ERROR( "something happend" );
 
     // parameters have no requirements to stay alive beyond the next statement
     std::string const params[] = { "hdr1 ", "hdr2", "3  " };
