4007ed937a0c63d0c114d5579f409a7d2f398559
Steven Watanabe <steven@providere-consulting.com>
2010-12-09 17:08:15 +0000

    Fix a few doc typos.  Fixes #4923.  Fixes #4924.
    [SVN r67134]

--- doc/src/examples/example10.cpp
+++ doc/src/examples/example10.cpp
@@ -8,7 +8,7 @@ typedef boost::mpl::list<int,long,unsigned char> test_types;
 
 BOOST_AUTO_TEST_CASE_TEMPLATE( my_test, T, test_types )
 {
-    BOOST_CHECK_EQUAL( sizeof(T), (unsigned)4 )
+    BOOST_CHECK_EQUAL( sizeof(T), (unsigned)4 );
 }
 
 //____________________________________________________________________________//
