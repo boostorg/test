a0894e6cb8d5e5306489fa7ad423bb6cb3605cbc
Gennadiy Rozental <rogeeff@gmail.com>
2011-10-03 20:21:32 +0000

    new decorator example
    [SVN r74673]

--- example/unit_test_example_04.cpp
+++ example/unit_test_example_04.cpp
@@ -12,15 +12,29 @@ namespace unittest=boost::unit_test;
 
 //____________________________________________________________________________//
 
+struct suite_fixture {
+    suite_fixture()     { BOOST_TEST_MESSAGE( "Running some test suite setup" ); }
+    ~suite_fixture()    { BOOST_TEST_MESSAGE( "Running some test suite teardown" ); }
+};
+
 // automatically registered test cases could be organized in test suites
+BOOST_TEST_DECORATOR(
++ unittest::fixture<suite_fixture>()
+)
 BOOST_AUTO_TEST_SUITE( my_suite1 )
 
 BOOST_AUTO_TEST_CASE_EXPECTED_FAILURES(my_test1,1);
+
+void some_setup()
+{
+    BOOST_TEST_MESSAGE( "Running some extra setup" );
+}
 
 BOOST_TEST_DECORATOR(
 + unittest::label( "L1" )
 + unittest::label( "L2" )
 + unittest::description( "test suite description" )
++ unittest::fixture( &some_setup )
 )
 BOOST_AUTO_TEST_CASE( my_test1 )
 {
@@ -84,6 +98,30 @@ BOOST_AUTO_TEST_CASE( my_test5 )
 
     BOOST_CHECK_EQUAL( i, 1 );
 }
+
+BOOST_TEST_DECORATOR(
+- unittest::enable_if( 100 < 50 )
+)
+BOOST_AUTO_TEST_CASE( my_test6 )
+{
+}
+
+BOOST_TEST_DECORATOR(
++ unittest::depends_on( "my_suite2/internal_suite/disabled_suite/my_test7" )
+)
+BOOST_AUTO_TEST_CASE( this_should_also_be_disabled )
+{
+}
+
+BOOST_TEST_DECORATOR(
+- unittest::disable_if( true )
+)
+BOOST_AUTO_TEST_SUITE( disabled_suite )
+
+BOOST_AUTO_TEST_CASE( my_test7 )
+{
+}
+BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
 
