7df28b8b1a097ef3b7587d0e134abc21a95c4260
Gennadiy Rozental <rogeeff@gmail.com>
2011-10-05 08:40:25 +0000

    moved in proper place
    [SVN r74718]

--- include/boost/test/unit_test.hpp
+++ include/boost/test/unit_test.hpp
@@ -43,12 +43,14 @@
 
 namespace boost { namespace unit_test {
 
+int BOOST_TEST_DECL unit_test_main( init_unit_test_func init_func, int argc, char* argv[] );
+
+}
+
 // !! ?? to remove
 namespace unit_test_framework=unit_test;
 
-int BOOST_TEST_DECL unit_test_main( init_unit_test_func init_func, int argc, char* argv[] );
-
-}}
+}
 
 #if defined(BOOST_TEST_DYN_LINK) && defined(BOOST_TEST_MAIN) && !defined(BOOST_TEST_NO_MAIN)
 
