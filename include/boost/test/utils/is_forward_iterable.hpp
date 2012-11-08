76b9f2e4c03dff36e8bd2d0a47d9ba3802327deb
Gennadiy Rozental <rogeeff@gmail.com>
2012-11-08 08:56:16 +0000

    temporary fix
    [SVN r81250]

--- include/boost/test/utils/is_forward_iterable.hpp
+++ include/boost/test/utils/is_forward_iterable.hpp
@@ -9,18 +9,27 @@
 //
 //  Version     : $Revision$
 //
-//  Description : defines a special purpose type trait
+//  Description : defines the is_forward_iterable collection type trait
 // ***************************************************************************
 
 #ifndef BOOST_TEST_IS_FRWARD_ITERABLE_HPP_110612GER
 #define BOOST_TEST_IS_FRWARD_ITERABLE_HPP_110612GER
 
+#if 1 // def BOOST_NO_CXX11_DECLTYPE
 // Boost
 #include <boost/mpl/bool.hpp>
+
 // STL
 #include <list>
 #include <vector>
 
+#else
+
+// STL
+#include <type_traits> 
+#include <utility> 
+
+#endif
 //____________________________________________________________________________//
 
 namespace boost {
@@ -30,6 +39,7 @@ namespace unit_test {
 // **************             is_forward_iterable              ************** //
 // ************************************************************************** //
 
+#if 1 // def BOOST_NO_CXX11_DECLTYPE
 template<typename T>
 struct is_forward_iterable : mpl::false_ {};
 
@@ -44,6 +54,34 @@ struct is_forward_iterable<std::vector<T>> : mpl::true_ {};
 
 template<typename T>
 struct is_forward_iterable<std::list<T>> : mpl::true_ {};
+
+#else
+
+namespace detail {
+
+struct is_forward_iterable_impl
+{ 
+    template<typename T, 
+             typename = decltype(std::declval<const T&>().size()),
+             typename = decltype(std::declval<T>().begin()), 
+             typename = typename T::const_iterator,
+             typename = typename T::value_type
+    > 
+    static std::true_type   test(int); 
+    template<typename> 
+    static std::false_type  test(...); 
+}; 
+
+
+} // namespace detail
+
+template<typename T> 
+struct is_forward_iterable 
+: decltype(detail::is_forward_iterable_impl::test<typename std::remove_reference<T>::type>(0))
+{ 
+}; 
+
+#endif
 
 } // namespace unit_test
 } // namespace boost
