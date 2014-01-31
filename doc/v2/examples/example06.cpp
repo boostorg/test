ed3293a22138bd27c7478f523a0835928e4b5675
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-01-31 16:33:52 +0100

    advances on examples

--- doc/v2/examples/example06.cpp
+++ doc/v2/examples/example06.cpp
@@ -1,12 +1,9 @@
+//[example06
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
 
-//____________________________________________________________________________//
-
 BOOST_AUTO_TEST_CASE( free_test_function )
 {
-    BOOST_CHECK( true /* test assertion */ );
+  BOOST_CHECK( true /* test assertion */ );
 }
-
-//____________________________________________________________________________//
-
+//]
