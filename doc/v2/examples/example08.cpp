1204b49fa87532fdf544bc1122cedb0c629e6afb
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-01-31 17:06:17 +0100

    advances on template test cases

--- doc/v2/examples/example08.cpp
+++ doc/v2/examples/example08.cpp
@@ -1,3 +1,4 @@
+//[example08
 #define BOOST_TEST_ALTERNATIVE_INIT_API
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
@@ -6,29 +7,25 @@
 using namespace boost::unit_test;
 using namespace boost;
 
-//____________________________________________________________________________//
-
-class test_class {
+class test_class
+{
 public:
-    void test_method( double const& d )
-    {
-        BOOST_CHECK_CLOSE( d * 100, (double)(int)(d*100), 0.01 );
-    }
+  void test_method( double const& d )
+  {
+    BOOST_CHECK_CLOSE( d * 100, (double)(int)(d*100), 0.01 );
+  }
 } tester;
-
-//____________________________________________________________________________//
 
 bool init_unit_test()
 {
-    double params[] = { 1., 1.1, 1.01, 1.001, 1.0001 };
+  double params[] = { 1., 1.1, 1.01, 1.001, 1.0001 };
 
-    callback1<double> tm = bind( &test_class::test_method, &tester, _1);
+  callback1<double> tm = bind( &test_class::test_method, &tester, _1);
 
-    framework::master_test_suite().
-        add( BOOST_PARAM_TEST_CASE( tm, params, params+5 ) );
+  framework::master_test_suite().
+    add( BOOST_PARAM_TEST_CASE( tm, params, params+5 ) );
 
-    return true;
+  return true;
 }
-
-//____________________________________________________________________________//
+//]
 
