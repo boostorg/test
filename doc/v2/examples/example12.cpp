e5ab8d447f05dd0e72410d58e0d066f343178e3b
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-02-02 02:06:26 +0100

    advances on the test organization

--- doc/v2/examples/example12.cpp
+++ doc/v2/examples/example12.cpp
@@ -1,3 +1,4 @@
+//[example12
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
 
@@ -5,13 +6,13 @@ BOOST_AUTO_TEST_SUITE( test_suite1 )
 
 BOOST_AUTO_TEST_CASE( test_case1 )
 {
-    BOOST_WARN( sizeof(int) < 4 );
+  BOOST_WARN( sizeof(int) < 4 );
 }
 
 BOOST_AUTO_TEST_CASE( test_case2 )
 {
-    BOOST_REQUIRE_EQUAL( 1, 2 );
-    BOOST_FAIL( "Should never reach this line" );
+  BOOST_REQUIRE_EQUAL( 1, 2 );
+  BOOST_FAIL( "Should never reach this line" );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -19,12 +20,13 @@ BOOST_AUTO_TEST_SUITE( test_suite2 )
 
 BOOST_AUTO_TEST_CASE( test_case3 )
 {
-    BOOST_CHECK( true );
+  BOOST_CHECK( true );
 }
 
 BOOST_AUTO_TEST_CASE( test_case4 )
 {
-    BOOST_CHECK( false );
+  BOOST_CHECK( false );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+//]
--- doc/v2/examples/example15.cpp
+++ doc/v2/examples/example15.cpp
@@ -1,21 +1,16 @@
+//[example15
 #include <boost/test/included/unit_test.hpp>
 using namespace boost::unit_test;
 
-//____________________________________________________________________________//
 
 BOOST_AUTO_TEST_CASE( free_test_function )
 {
-    BOOST_CHECK( true /* test assertion */ );
+  BOOST_CHECK( true /* test assertion */ );
 }
 
-//____________________________________________________________________________//
-
-test_suite*
-init_unit_test_suite( int argc, char* argv[] ) 
+test_suite* init_unit_test_suite( int argc, char* argv[] )
 {
-    framework::master_test_suite().p_name.value = "my master test suite name";
-
-    return 0;
+  framework::master_test_suite().p_name.value = "my master test suite name";
+  return 0;
 }
-
-//____________________________________________________________________________//
+//]
