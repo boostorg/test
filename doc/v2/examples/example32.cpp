89c0aa76ffdd6b974ddd3750188f753acfc7b802
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-02-11 10:52:16 +0100

    cleaning example 32 links to checkpoint and passpoint fixing links

--- doc/v2/examples/example32.cpp
+++ doc/v2/examples/example32.cpp
@@ -3,7 +3,6 @@
 #include <boost/test/included/unit_test.hpp>
 #include <utility>
 
-//____________________________________________________________________________//
 
 typedef std::pair<int,float> pair_type;
 
@@ -11,11 +10,9 @@ BOOST_TEST_DONT_PRINT_LOG_VALUE( pair_type );
 
 BOOST_AUTO_TEST_CASE( test_list )
 {
-    pair_type p1( 2, 5.5 );
-    pair_type p2( 2, 5.501 );
+  pair_type p1( 2, 5.5 );
+  pair_type p2( 2, 5.501 );
 
-    BOOST_CHECK_EQUAL( p1, p2 );
+  BOOST_CHECK_EQUAL( p1, p2 );
 }
-
-//____________________________________________________________________________//
 //]
