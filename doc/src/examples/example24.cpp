f7858d8c5a17d17fa6ff999f3901755d03c0547c
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-02-23 22:45:43 +0100

    example 24

--- doc/src/examples/example24.cpp
+++ doc/src/examples/example24.cpp
@@ -1,13 +1,10 @@
+//[example24
 #include <iostream>
 #include <boost/test/included/prg_exec_monitor.hpp> 
 
-//____________________________________________________________________________//
-
 int cpp_main( int, char* [] ) // note name cpp_main, not main.
 {
-    std::cout << "Hello, world\n";
-
-    return 0;
+  std::cout << "Hello, world\n";
+  return 0;
 }
-
-//____________________________________________________________________________//
+//]
