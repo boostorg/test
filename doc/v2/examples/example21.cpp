185c20bee27453e64603aea5f868d45a16566a03
Raffi Enficiaud <raffi.enficiaud@tue.mpg.de>
2014-03-04 00:51:37 +0100

    nicer code and indentation

--- doc/v2/examples/example21.cpp
+++ doc/v2/examples/example21.cpp
@@ -2,26 +2,20 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
 
-//____________________________________________________________________________//
-
 BOOST_AUTO_TEST_CASE( test_init )
 {
-    int current_time = 0; // real call is required here
+  int current_time = 0; // real call is required here
 
-    BOOST_TEST_MESSAGE( "Testing initialization :" );
-    BOOST_TEST_MESSAGE( "Current time:" << current_time );
+  BOOST_TEST_MESSAGE( "Testing initialization :" );
+  BOOST_TEST_MESSAGE( "Current time:" << current_time );
 }
-
-//____________________________________________________________________________//
 
 BOOST_AUTO_TEST_CASE( test_update )
 {
-    std::string field_name = "Volume";
-    int         value      = 100;
+  std::string field_name = "Volume";
+  int         value      = 100;
 
-    BOOST_TEST_MESSAGE( "Testing update :" );
-    BOOST_TEST_MESSAGE( "Update " << field_name << " with " << value );
+  BOOST_TEST_MESSAGE( "Testing update :" );
+  BOOST_TEST_MESSAGE( "Update " << field_name << " with " << value );
 }
-
-//____________________________________________________________________________//
 //]
--- doc/v2/examples/example51.cpp
+++ doc/v2/examples/example51.cpp
@@ -4,15 +4,11 @@
 #include <boost/test/detail/unit_test_parameters.hpp>
 using namespace boost::unit_test;
 
-//____________________________________________________________________________//
-
 BOOST_AUTO_TEST_CASE( test_case0 )
 {
-    if( runtime_config::log_level() < log_warnings )
-        unit_test_log.set_threshold_level( log_warnings );
+  if( runtime_config::log_level() < log_warnings )
+    unit_test_log.set_threshold_level( log_warnings );
 
-    BOOST_WARN( sizeof(int) > 4 );
+  BOOST_WARN( sizeof(int) > 4 );
 }
-
-//____________________________________________________________________________//
 //]
