b5d8b9f0c8b44ef2c099abb2086517413ef3b6fa
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2014-04-29 10:10:00 +0200

    silencing warnings on clang

--- include/boost/test/utils/runtime/validation.hpp
+++ include/boost/test/utils/runtime/validation.hpp
@@ -46,10 +46,12 @@ class logic_error
 public:
     // Constructor // !! could we eliminate shared_ptr
     explicit    logic_error( cstring msg ) : m_msg( new dstring( msg.begin(), msg.size() ) ) {}
-    ~logic_error() throw()                          {}
+    ~logic_error() BOOST_TEST_NOEXCEPT_OR_THROW_VOID 
+    {}
 
     dstring const&   msg() const                    { return *m_msg; }
-    virtual char_type const* what() const throw()   { return m_msg->c_str(); }
+    virtual char_type const* what() const BOOST_TEST_NOEXCEPT_OR_THROW_VOID
+    { return m_msg->c_str(); }
 
 private:
     dstring_ptr m_msg;
