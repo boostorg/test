e057ffd053d696ea36d30e3ca22b58366734283d
Jurko <jurko.gospodnetic@pke.hr>
2014-10-23 18:00:45 +0200

    add new Boost.Test command-line argument parsing unit test module
    Currently implemented tests are still far from a complete test suite but they do
    represent a good starting point for adding related tests in the future.

---
+++ test/test_cla_parsing.cpp
@@ -0,0 +1,147 @@
+//  (C) Copyright Jurko Gospodnetić 2014.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+//  File        : $RCSfile$
+//
+//  Version     : $Revision$
+//
+//  Description : unit test for UTF command-line argument parsing
+// ***************************************************************************
+
+// disable MSVC warnings about std::strcpy() being unsafe
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS 1
+#endif
+
+// Boost.Test
+#define BOOST_TEST_MODULE Boost.Test command-line argument parsing test
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_parameters.hpp>
+
+#include <cstring>
+#include <vector>
+
+namespace utf = boost::unit_test;
+
+// Test utility class for checking UTF command-line argument processing results.
+//
+class ArgChecker
+{
+public:
+    ArgChecker(int const input_data_count, char const * const input_data[])
+        :
+        argc_(input_data_count)
+    {
+        BOOST_REQUIRE_GT(input_data_count, 0);
+        BOOST_REQUIRE(input_data);
+
+        // calculate required argument data size and offsets
+        std::vector<std::size_t> data_offset(input_data_count);
+        std::size_t data_size = 0;
+        for (int i = 0; i < input_data_count; ++i) {
+            data_offset[i] = data_size;
+            data_size += strlen(input_data[i]) + 1;
+        }
+
+        // prepare argument data (data + argv)
+        data_.resize(data_size);
+        argv_.resize(input_data_count + 1);
+        for (int i = 0; i < input_data_count; ++i) {
+            argv_[i] = &data_[data_offset[i]];
+            std::strcpy(argv_[i], input_data[i]);
+        }
+
+        // argv[argc] must be NULL according to ANSI C standard
+        argv_[argc_] = NULL;
+
+        // save original data & argv content
+        saved_argc_ = argc_;
+        saved_data_ = data_;
+        saved_argv_ = argv_;
+
+        // run actual UTF command-line argument processing
+        utf::runtime_config::init(argc_, &argv_[0]);
+    }
+
+    // Check that the command-line arguments have not been changed while
+    // processed by UTF. UTF may replace them with copies, but their content
+    // must remain the same.
+    //
+    void check_unchanged() const
+    {
+        check(saved_argc_, &saved_argv_[0]);
+    }
+
+    // Check that the command-line arguments left behind after being processed
+    // by UTF.
+    //
+    void check(int const expected_argc,
+        char const * const expected_argv[]) const
+    {
+        BOOST_CHECK(saved_data_ == data_);  // input data unchanged
+        BOOST_CHECK_EQUAL(expected_argc, argc_);
+        if (expected_argc == argc_)
+            for (int i = 0; i < argc_; ++i)
+                BOOST_CHECK(!std::strcmp(expected_argv[i], argv_[i]));
+    }
+
+private:
+    ArgChecker(ArgChecker const &);
+    void operator=(ArgChecker const &) const;
+
+private:
+    int argc_;
+    std::vector<char> data_;
+    std::vector<char *> argv_;
+    int saved_argc_;
+    std::vector<char> saved_data_;
+    std::vector<char *> saved_argv_;
+};
+
+//____________________________________________________________________________//
+
+BOOST_AUTO_TEST_SUITE(empty_argument)
+
+    BOOST_AUTO_TEST_CASE(test_inside)
+    {
+        char * argv[] = {"a.exe", "x", "", "y"};
+        ArgChecker arg_checker(sizeof(argv)/sizeof(argv[0]), argv);
+        arg_checker.check_unchanged();
+    }
+
+    BOOST_AUTO_TEST_CASE(test_inside_sequence)
+    {
+        char * argv[] = {"a.exe", "x", "", "", "y"};
+        ArgChecker arg_checker(sizeof(argv)/sizeof(argv[0]), argv);
+        arg_checker.check_unchanged();
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+//____________________________________________________________________________//
+
+BOOST_AUTO_TEST_SUITE(non_empty_arguments_without_spaces)
+
+    BOOST_AUTO_TEST_CASE(test_one)
+    {
+        char const * const argv[] = {"a.exe", "x"};
+        ArgChecker arg_checker(sizeof(argv)/sizeof(argv[0]), argv);
+        arg_checker.check_unchanged();
+    }
+
+    BOOST_AUTO_TEST_CASE(test_multiple)
+    {
+        char * argv[] = {"a.exe", "x", "y"};
+        ArgChecker arg_checker(sizeof(argv)/sizeof(argv[0]), argv);
+        arg_checker.check_unchanged();
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+//____________________________________________________________________________//
+
+// EOF
