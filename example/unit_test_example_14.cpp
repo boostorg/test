10d0582c25f24d902435e9299ad79ad594f0efb5
Raffi Enficiaud <raffi.enficiaud@free.fr>
2014-12-11 10:58:59 +0100

    Licenses warnings

--- example/unit_test_example_14.cpp
+++ example/unit_test_example_14.cpp
@@ -1,3 +1,12 @@
+//  (C) Copyright Gennadiy Rozental 2001-2014.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+// ***************************************************************************
+
 #define BOOST_TEST_TOOL_REPORT_WARN_FAILURE( assertion_descr ) \
     std::cout << "Condition " << assertion_descr << " is not satisfied\n"
 
--- test/auto-link-test/auto_link_with_utf.cpp
+++ test/auto-link-test/auto_link_with_utf.cpp
@@ -1,14 +1,18 @@
-// Boost.Test
+//  (C) Copyright Gennadiy Rozental 2001-2014.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+// ***************************************************************************
+
+
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
-
-//____________________________________________________________________________//
 
 BOOST_AUTO_TEST_CASE( test_something )
 {
     BOOST_CHECK_EQUAL( 1, 1 );
 }
 
-//____________________________________________________________________________//
-
-// EOF
