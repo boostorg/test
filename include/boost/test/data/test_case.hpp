a613fe043996da080231106cb8203677f7dde472
Raffi Enficiaud <raffi.enficiaud@free.fr>
2014-12-16 14:42:32 +0100

    full namespace definition

--- include/boost/test/data/test_case.hpp
+++ include/boost/test/data/test_case.hpp
@@ -152,7 +152,7 @@ BOOST_AUTO_TU_REGISTRAR( test_name )(                                   \
     boost::unit_test::data::ds_detail::make_test_case_gen<test_name>(   \
           BOOST_STRINGIZE( test_name ),                                 \
           __FILE__, __LINE__,                                           \
-          data::make(dataset) ),                                        \
+          boost::unit_test::data::make(dataset) ),                      \
     boost::unit_test::decorator::collector::instance() );               \
                                                                         \
     template<BOOST_PP_ENUM_PARAMS(arity, typename Arg)>                 \
