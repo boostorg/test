bd2c0e7f7b34b780ebf5fd6ee690d5480bd96a4d
Gennadiy Rozental <rogeeff@gmail.com>
2014-12-30 11:55:03 -0500

    deleted img; more progress

--- doc/v2/snippet/5mnstarter/test_file.cpp
+++ doc/v2/snippet/5mnstarter/test_file.cpp
@@ -1,11 +1,11 @@
-#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE My Test
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(first_test)
 {
-  BOOST_CHECK(true);
+  BOOST_TEST(true);
   
   int i = 1;
-  BOOST_CHECK_EQUAL(i, 1);
+  BOOST_TEST(i == 2);
 }
 
