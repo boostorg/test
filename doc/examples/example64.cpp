6eab70d0b66bf35268970bc4e288914c503f37a6
Raffi Enficiaud <raffi.enficiaud@free.fr>
2015-01-03 15:26:07 +0100

    Example of cartesian product using a range, does not work

--- doc/examples/example64.cpp
+++ doc/examples/example64.cpp
@@ -16,20 +16,23 @@ namespace bdata = boost::unit_test::data;
 
 BOOST_DATA_TEST_CASE( 
   test_case_snippet_1, 
-  bdata::make(1, 17) ^ bdata::xrange(7), 
-  random_sample, index )
+  bdata::xrange(3) * bdata::xrange(4), 
+  d1, d2)
 {
-  std::cout << "test 1 " << random_sample << std::endl;
-  BOOST_CHECK(random_sample <= 17 && random_sample >= 1);
+  //std::cout << "test 1 " << d1 << ", " << d2 << std::endl;
+  BOOST_CHECK(d1 <= 3);// && d2 <= 4);
 }
 
+
+#if 0
 BOOST_DATA_TEST_CASE( 
   test_case_snippet_2, 
   bdata::random( (bdata::distribution=std::uniform_real_distribution<float>(1, 2)) ) 
-      ^ bdata::xrange(7),
-  random_sample, index )
+      * bdata::xrange(7),
+  random_sample, d2 )
 {
   std::cout << "test 2 " << random_sample << std::endl;
   BOOST_CHECK(random_sample < 1.7); // 30% chance of failure
 }
+#endif
 //]
