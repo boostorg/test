9d37963bc64ff9ef13479da3783b011724340c26
Raffi Enficiaud <raffi.enficiaud@free.fr>
2015-01-08 17:57:27 +0100

    One more example

--- doc/snippet/dataset_1/test_file.cpp
+++ doc/snippet/dataset_1/test_file.cpp
@@ -13,26 +13,24 @@ namespace data=boost::unit_test::data;
 
 #if 0
 //[snippet_dataset1_1
-BOOST_DATA_TEST_CASE( test_case_1, data::xrange(5) )
+BOOST_DATA_TEST_CASE( test_case_arity1_implicit, data::xrange(5) )
 {
-  BOOST_TEST_MESSAGE(sample);
   BOOST_CHECK(sample <= 4 && sample >= 0);
 }
 //]
 
 //[snippet_dataset1_2
-BOOST_DATA_TEST_CASE( test_case_1_1, data::xrange(5), my_var )
+BOOST_DATA_TEST_CASE( test_case_arity1, data::xrange(5), my_var )
 {
-  BOOST_TEST_MESSAGE(my_var);
   BOOST_CHECK(my_var <= 4 && my_var >= 0);
 }
 //]
 
 //[snippet_dataset1_3
-BOOST_DATA_TEST_CASE( test_case_2, data::xrange(5) ^ data::xrange(5), var1, var2)
+BOOST_DATA_TEST_CASE( test_case_arity2, data::xrange(2) ^ data::xrange(5), apples, potatoes)
 {
-  BOOST_TEST_MESSAGE(var1 << " - " << var2);
-  BOOST_CHECK(true);
+  BOOST_CHECK(apples <= 1 && apples >= 0);
+  BOOST_CHECK(potatoes <= 4 && potatoes >= 0);
 }
 //]
 
