6d7daf6b4534aa688e2bc47ff2aa46416e91e2da
Andrzej Krzemienski <akrzemi1@gmail.com>
2015-02-03 00:00:45 +0100

    docs: updated decorators doc to match develop

--- doc/examples/example78_decorators.cpp
+++ doc/examples/example78_decorators.cpp
@@ -6,28 +6,17 @@
 //  See http://www.boost.org/libs/test for the library home page.
 
 //[example_code
-#define BOOST_TEST_MODULE example
+#define BOOST_TEST_MODULE example78
 #include <boost/test/unit_test.hpp>
+
 namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_SUITE( suite1 )
-
-  BOOST_AUTO_TEST_CASE( test_case1 )
-  {
-    BOOST_TEST(true);
-  }
-
-BOOST_AUTO_TEST_SUITE_END()
-
 BOOST_TEST_DECORATOR(
-  + utf::label("trivial")
+  + utf::timeout(2)
 )
-BOOST_AUTO_TEST_SUITE( suite1 )
-
-  BOOST_AUTO_TEST_CASE( test_case2 )
-  {
-    BOOST_TEST(1 == 1);
-  }
-
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE( test_case1 )
+{
+  for(;;) {}
+  BOOST_TEST(true);
+}
 //]
