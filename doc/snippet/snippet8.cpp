a964366d5a27eca7d4e94ff609f11f227b5566d7
Raffi Enficiaud <raffi.enficiaud@free.fr>
2015-02-03 02:10:30 +0100

    Some cleanup over the snippet files Adding the licenses as suggested by the introspection tool

--- doc/snippet/snippet8.cpp
+++ doc/snippet/snippet8.cpp
@@ -1,3 +1,13 @@
+//  (C) Copyright Gennadiy Rozental 2001-2015.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+//  See http://www.boost.org/libs/test for the library home page.
+//
+
+
+//[snippet8
 double find_root( double             (*f)(double), 
                   double               low_guess, 
                   double               high_guess, 
@@ -34,3 +44,4 @@ double find_root( double             (*f)(double),
     
     return solution;
 }
+//]
