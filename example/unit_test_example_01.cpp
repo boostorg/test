588a3c1187af46c5c7005243ec673ac11f563e2a
Gennadiy Rozental <rogeeff@gmail.com>
2015-02-14 17:08:52 -0500

    revert

--- example/unit_test_example_01.cpp
+++ example/unit_test_example_01.cpp
@@ -1,23 +1,39 @@
-#define BOOST_TEST_MODULE example
+//  (C) Copyright Gennadiy Rozental 2005.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+
+// Boost.Test
+
+// each test module could contain no more then one 'main' file with init function defined
+// alternatively you could define init function yourself
+#define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
-namespace utf = boost::unit_test;
+//____________________________________________________________________________//
 
-BOOST_TEST_DECORATOR(
-  + utf::timeout(2)
-)
-BOOST_AUTO_TEST_SUITE( suite1 )
+// most frequently you implement test cases as a free functions with automatic registration
+BOOST_AUTO_TEST_CASE( test1 )
+{
+    // reports 'error in "test1": test 2 == 1 failed'
+    BOOST_CHECK( 2 == 1 );
+}
 
-  BOOST_AUTO_TEST_CASE( test_case1 )
-  {
-    for(;;) {}
-    BOOST_TEST(true);
-  }
+//____________________________________________________________________________//
 
-  BOOST_AUTO_TEST_CASE( test_case2 )
-  {
-    while(true) {}
-    BOOST_TEST(true);
-  }
+// each test file may contain any number of test cases; each test case has to have unique name
+BOOST_AUTO_TEST_CASE( test2 )
+{
+    int i = 0;
 
-BOOST_AUTO_TEST_SUITE_END()
+    // reports 'error in "test2": check i == 2 failed [0 != 2]'
+    BOOST_CHECK_EQUAL( i, 2 );
+
+    BOOST_CHECK_EQUAL( i, 0 );
+}
+
+//____________________________________________________________________________//
+
+// EOF
