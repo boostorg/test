0974a6b4a47aef635dc1f1bf270248ce3825a9b3
Raffi Enficiaud <raffi.enficiaud@free.fr>
2015-05-09 01:25:16 +0200

    BOOST_TEST doc updates - floating point comparison - composition rule for the BOOST_TEST - container comparison

--- doc/examples/boost_test_macro_container_float.cpp
+++ doc/examples/boost_test_macro_container_float.cpp
@@ -6,17 +6,20 @@
 //  See http://www.boost.org/libs/test for the library home page.
 
 //[example_code
-#define BOOST_TEST_MODULE boost_test_sequence_float
+#define BOOST_TEST_MODULE boost_test_macro_container_float
 #include <boost/test/included/unit_test.hpp>
 #include <vector>
 
-BOOST_AUTO_TEST_CASE( test_collections_vectors_floats )
+BOOST_AUTO_TEST_CASE( test_container_vectors_floats )
 {
   std::vector<float> a{0.1f, 0.2f, 0.3f};
   std::vector<double> b{0.11, 0.2, 0.29};
   
   BOOST_TEST(a == b); // fails
   BOOST_TEST(a == b, 10. % boost::test_tools::tolerance()); // pass
+  BOOST_TEST(a <= b, 10. % boost::test_tools::tolerance()); // pass
+  BOOST_TEST(a > b, 10. % boost::test_tools::tolerance()); // pass
+  BOOST_TEST(a < b, (10. % boost::test_tools::tolerance())
+                    << boost::test_tools::per_element() ); // pass
 }
-
 //]
