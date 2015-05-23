52ff1e77f7a371405d83c4d6d2bf2229298c418c
Gennadiy Rozental <rogeeff@gmail.com>
2015-05-22 23:09:46 -0400

    intro/tutorials update

--- doc/snippet/snippet17.cpp
+++ doc/snippet/snippet17.cpp
@@ -15,17 +15,17 @@ __BOOST_AUTO_TEST_CASE__( constructors_test )
   //...
 }
 
-BOOST_AUTO_TEST_CASE( data_access_test )
+__BOOST_AUTO_TEST_CASE__( data_access_test )
 {
-  const_string cs1( "test_string" );                                 // 1 //
-  __BOOST_CHECK_EQUAL__( cs1[(size_t)0], 't' );
-  BOOST_CHECK_EQUAL( cs1[(size_t)4], '_' );
-  BOOST_CHECK_EQUAL( cs1[cs1.length()-1], 'g' );
+  const_string cs1( "test_string" );                                // 1 //
+  __BOOST_TEST__( cs1[(size_t)0] == 't' );
+  __BOOST_TEST__( cs1[(size_t)4] == '_' );
+  __BOOST_TEST__( cs1[cs1.length()-1] == 'g' );
 
-  BOOST_CHECK_EQUAL( cs1[(size_t)0], cs1.at( 0 ) );                  // 2 //
-  BOOST_CHECK_EQUAL( cs1[(size_t)2], cs1.at( 5 ) );
-  BOOST_CHECK_EQUAL( cs1.at( cs1.length() - 1 ), 'g' );
+  __BOOST_TEST__( cs1[(size_t)0] == cs1.at( 0 ) );                      // 2 //
+  __BOOST_TEST__( cs1[(size_t)2] == cs1.at( 5 ) );
+  __BOOST_TEST__( cs1.at( cs1.length() - 1 ) == 'g' );
 
-  BOOST_CHECK_THROW( cs1.at( cs1.length() ), std::out_of_range );    // 3 //
+  BOOST_CHECK_THROW( cs1.at( cs1.length() ), std::out_of_range );   // 3 //
 }
 //]
