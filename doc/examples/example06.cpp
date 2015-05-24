6c6769ae71a90481ce6896661f3b756f7656d8c3
Gennadiy Rozental <rogeeff@gmail.com>
2015-05-24 05:58:57 -0400

    nullary test case documentation update; web wisdom page added

--- doc/examples/example06.cpp
+++ doc/examples/example06.cpp
@@ -10,6 +10,7 @@
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( free_test_function )
+/* Compare with void free_test_function() */
 {
   BOOST_TEST( true /* test assertion */ );
 }
