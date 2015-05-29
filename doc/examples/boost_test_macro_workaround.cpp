3d9974cc92505ed9af206e74dcc066e742c588ab
Raffi Enficiaud <raffi.enficiaud@free.fr>
2015-05-29 10:32:08 +0200

    More about workarounds

--- doc/examples/boost_test_macro_workaround.cpp
+++ doc/examples/boost_test_macro_workaround.cpp
@@ -6,19 +6,9 @@
 //  See http://www.boost.org/libs/test for the library home page.
 
 //[example_code
-#define BOOST_TEST_MODULE boost_test_macro1
+#define BOOST_TEST_MODULE boost_test_macro_workaround
 #include <boost/test/included/unit_test.hpp>
 #include <sstream>
-
-BOOST_AUTO_TEST_CASE( test_basic_operations )
-{
-  int a = 1;
-  int b = 1;
-  BOOST_TEST(a == b);
-  BOOST_TEST(a < b + 1);
-  BOOST_TEST(b >= a);
-  BOOST_TEST(!(b >= b + 1));
-}
 
 BOOST_AUTO_TEST_CASE( test_logical_not_allowed )
 {
@@ -26,4 +16,15 @@ BOOST_AUTO_TEST_CASE( test_logical_not_allowed )
   // BOOST_TEST(true && true);
   BOOST_TEST((true && true)); // with extra brackets, it works as expected
 }
+
+BOOST_AUTO_TEST_CASE( test_ternary )
+{
+  int a = 1;
+  int b = 2;
+  // Boost Unit Test Framework prevents compilation of
+  // BOOST_TEST(a == b ? true : false);
+  BOOST_TEST((a + 1 == b ? true : false)); // again works as expected with extra brackets
+}
+
+
 //]
