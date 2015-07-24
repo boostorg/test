3af4b3510bc9fdb3060d43e353d5ea46fdb8c98d
Gennadiy Rozental <rogeeff@gmail.com>
2015-07-24 14:46:26 -0400

    work in progress

---
+++ test/utils-ts/cla-parser-test.cpp
@@ -0,0 +1,32 @@
+//  (C) Copyright Gennadiy Rozental 2015.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+//  File        : $RCSfile$
+//
+//  Version     : $Revision$
+//
+//  Description : unit test for comamnd line parameter parsing framework
+// ***************************************************************************
+
+// Boost.Test
+#define BOOST_TEST_MODULE Boost.Test CLA parser test
+#include <boost/test/unit_test.hpp>
+namespace utf = boost::unit_test;
+
+// STL
+
+BOOST_AUTO_TEST_SUITE( test_argv_traverser )
+
+BOOST_AUTO_TEST_CASE( test_construction )
+{
+}
+
+//____________________________________________________________________________//
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// EOF
