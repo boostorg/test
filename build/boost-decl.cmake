1d01166efb7e2309131dd2ed7a16e003e14f80c3
Raffi Enficiaud <raffi.enficiaud@free.fr>
2017-07-24 00:09:13 +0200

    More cmake

--- build/boost-decl.cmake
+++ build/boost-decl.cmake
@@ -1,8 +1,13 @@
 # boost-decl.cmake
 # declares the components of boost.test and their dependencies
 
+# The following variables should be defined:
+#
+# * ``BOOST_LIB_<current_package>_COMPONENTS``: list indicating all the components of the current package
+# * ``BOOST_LIB_<current_package>_COMPONENTS_<component>_DEPENDENCY`` list indicating all the dependencies of the ``component``
+
 set(_current_package "TEST")
-set(BOOST_LIB_${_current_package}_COMPONENTS "build" "test") #"doc" "test")
+set(BOOST_LIB_${_current_package}_COMPONENTS "build" "doc" "test")
 
 # checking no aliasing
 foreach(_component IN LISTS BOOST_LIB_${_current_package}_COMPONENTS)
@@ -15,7 +20,3 @@ endforeach()
 set(BOOST_LIB_${_current_package}_COMPONENTS_BUILD_DEPENDENCY ) #"system:build" "core:build")
 set(BOOST_LIB_${_current_package}_COMPONENTS_DOC_DEPENDENCY ) #"quickbook:core")
 set(BOOST_LIB_${_current_package}_COMPONENTS_TEST_DEPENDENCY "test:build")
-
-unset(_current_package)
-
-#message(FATAL_ERROR "${BOOST_LIB_${_current_package}_COMPONENTS_BUILD_DEPENDENCY}")
