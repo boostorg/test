ce1020043b5819a23660667e2b4530f6ab4f4157
Raffi Enficiaud <raffi.enficiaud@free.fr>
2017-07-30 03:46:13 +0200

    New cmake

--- build/boost-decl.cmake
+++ build/boost-decl.cmake
@@ -17,6 +17,28 @@ foreach(_component IN LISTS BOOST_LIB_${_current_package}_COMPONENTS)
   endif()
 endforeach()
 
-set(BOOST_LIB_${_current_package}_COMPONENTS_BUILD_DEPENDENCY ) #"system:build" "core:build")
+set(BOOST_LIB_${_current_package}_COMPONENTS_BUILD_DEPENDENCY
+  "system:build"
+  "core:build"
+  "config:build"
+  "predef:build"
+  "assert:build"
+  "function:build"
+  "mpl:build"
+  "throw_exception:build"
+  "static_assert:build"
+  "preprocessor:build"
+  "functional:build"
+  "utility:build"
+  "smart_ptr:build"
+  "bind:build"
+  "exception:build"
+  "iterator:build"
+  "move:build"
+  "timer:build"
+  "io:build"
+  "algorithm:build"
+  "range:build"
+)
 set(BOOST_LIB_${_current_package}_COMPONENTS_DOC_DEPENDENCY ) #"quickbook:core")
 set(BOOST_LIB_${_current_package}_COMPONENTS_TEST_DEPENDENCY "test:build")
