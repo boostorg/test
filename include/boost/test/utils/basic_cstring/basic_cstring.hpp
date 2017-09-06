67f96729c94df716a9911e08f0fbc3e4bc662c61
Brian Kuhl <brian.kuhl@windriver.com>
2017-09-05 23:19:23 -0400

    Diab compiler has limit on size of enum
    Similar to the IBM compiler

--- include/boost/test/utils/basic_cstring/basic_cstring.hpp
+++ include/boost/test/utils/basic_cstring/basic_cstring.hpp
@@ -60,7 +60,7 @@ public:
 
     // !! should also present reverse_iterator, const_reverse_iterator
 
-#if !BOOST_WORKAROUND(__IBMCPP__, BOOST_TESTED_AT(600))
+#if !BOOST_WORKAROUND(__IBMCPP__, BOOST_TESTED_AT(600)) || !defined(__DCC__)
     enum npos_type { npos = static_cast<size_type>(-1) };
 #else
     // IBM/VisualAge version 6 is not able to handle enums larger than 4 bytes.
