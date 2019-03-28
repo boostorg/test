04006ad5bdab92ad3f0eb23fb264a914173191eb
Raffi Enficiaud <raffi.enficiaud@mines-paris.org>
2019-03-28 07:19:19 +0100

    Fix visibility warnings
    Forward declarations for templates should also have the visibility right
    Local static variables cannot have their visibility set and need to get out of the function scope.

--- include/boost/test/detail/config.hpp
+++ include/boost/test/detail/config.hpp
@@ -78,6 +78,7 @@ class type_info;
 
 //____________________________________________________________________________//
 
+// Sun compiler does not support visibility on enums
 #if defined(__SUNPRO_CC)
 #define BOOST_TEST_ENUM_SYMBOL_VISIBLE
 #else
