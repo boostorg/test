187c336fdd883d14408dbea2c21cc2d498374280
Raffi Enficiaud <raffi.enficiaud@free.fr>
2019-11-20 21:19:56 +0100

    Playing

---
+++ tools/clang-tools/README.md
@@ -0,0 +1,5 @@
+Attempt to use the clang-tool for partial preprocessing
+
+```
+sudo apt-get install libclang-8-dev
+```
--- tools/clang-tools/main.cpp
+++ tools/clang-tools/main.cpp
@@ -7,8 +7,15 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/Utils.h"
 
+#include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+
 using namespace clang::tooling;
 using namespace llvm;
+using namespace clang::ast_matchers;
+
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
@@ -25,47 +32,57 @@ static cl::extrahelp MoreHelp("\nMore help text...\n");
 void
 preprocess( const std::string& infile, const std::string& outf )
 {
- clang::CompilerInstance inst;
- std::unique_ptr<raw_pwrite_stream> myos = inst.createOutputFile(
-  outf,
-  false,
-  true,
-  "",
-  ".i",
-  false);
+    clang::CompilerInstance inst;
+    std::unique_ptr<raw_pwrite_stream> myos
+        = inst.createOutputFile(outf, false, true, "", ".i", false);
 
- inst.createFileManager();
- inst.createDiagnostics();
- clang::TargetOptions& options = inst.getTargetOpts();
- options.Triple = "i486-ubuntu-linux";
+    inst.createFileManager();
+    inst.createDiagnostics();
+    clang::TargetOptions &options = inst.getTargetOpts();
+    options.Triple = "i486-ubuntu-linux";
 
- clang::TargetInfo* target = clang::TargetInfo::CreateTargetInfo(
-      inst.getDiagnostics(),
-      std::make_shared<clang::TargetOptions>(options)); // not sure
- inst.setTarget(target);
- inst.createSourceManager(inst.getFileManager());
- inst.InitializeSourceManager(clang::FrontendInputFile(infile, clang::InputKind(clang::InputKind::CXX)));
- clang::PreprocessorOptions& opts = inst.getPreprocessorOpts();
- //Push back all of the options...the defs can be “FOO=val”
- //opts.addMacroDef( … );
- inst.createPreprocessor(clang::TranslationUnitKind::TU_Complete);
-  std::shared_ptr<clang::Preprocessor> preprocessor = inst.getPreprocessorPtr();
- clang::DoPrintPreprocessedInput(
-  *preprocessor,
-  &(*myos),
-  inst.getPreprocessorOutputOpts() );
+    clang::TargetInfo *target
+        = clang::TargetInfo::CreateTargetInfo(inst.getDiagnostics(),
+                                              std::make_shared<clang::TargetOptions>(
+                                                  options)); // not sure
+    inst.setTarget(target);
+    inst.createSourceManager(inst.getFileManager());
+    inst.InitializeSourceManager(
+        clang::FrontendInputFile(infile, clang::InputKind(clang::InputKind::CXX)));
+    clang::PreprocessorOptions &opts = inst.getPreprocessorOpts();
+    //Push back all of the options...the defs can be “FOO=val”
+    //opts.addMacroDef( … );
+    inst.createPreprocessor(clang::TranslationUnitKind::TU_Complete);
+    std::shared_ptr<clang::Preprocessor> preprocessor = inst.getPreprocessorPtr();
+    clang::DoPrintPreprocessedInput(*preprocessor, &(*myos), inst.getPreprocessorOutputOpts());
 
- //myos->close();
+    //myos->close();
 }
 
-int main(int argc, const char **argv) {
-  preprocess("/Users/raffi/code/boost/libs/test/include/boost/test/impl/framework.ipp", "/tmp/toto.cpp");
 
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
-  ClangTool Tool(OptionsParser.getCompilations(),
-                 OptionsParser.getSourcePathList());
-#if 0
-  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
-#endif
-return 0;
+StatementMatcher LoopMatcher =
+  forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(
+    hasInitializer(integerLiteral(equals(0)))))))).bind("forLoop");
+
+class LoopPrinter : public MatchFinder::MatchCallback {
+public:
+    virtual void run(const MatchFinder::MatchResult &Result)
+    {
+        if (const clang::ForStmt *FS = Result.Nodes.getNodeAs<clang::ForStmt>("forLoop"))
+            FS->dump();
+    }
+};
+
+int main(int argc, const char **argv)
+{
+    //preprocess("/media/raffi/linux-data/code/perso/boost/libs/test/include/boost/test/impl/framework.ipp", "/tmp/toto.cpp");
+
+    CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+    ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
+
+    LoopPrinter Printer;
+    MatchFinder Finder;
+    Finder.addMatcher(LoopMatcher, &Printer);
+
+    return Tool.run(newFrontendActionFactory(&Finder).get());
 }
