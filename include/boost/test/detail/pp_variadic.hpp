08d9bb8abc5ef0e4561758477b0ca29e30b2e200
Raffi Enficiaud <raffi.enficiaud@free.fr>
2020-07-16 21:40:13 +0200

    starting the work on replacing the BOOST_PP_XXXX

--- include/boost/test/detail/pp_variadic.hpp
+++ include/boost/test/detail/pp_variadic.hpp
@@ -12,10 +12,47 @@
 #ifndef BOOST_TEST_PP_VARIADIC_HPP_021515GER
 #define BOOST_TEST_PP_VARIADIC_HPP_021515GER
 
+
+#if BOOST_TEST_PP_IMPL
+
+// BOOST_TEST_PP_NARG returns the number of arguments of a variable argument list
+// https://stackoverflow.com/a/2124385/1617295
+#define BOOST_TEST_PP_NARG(...)     BOOST_TEST_PP_NARG_(__VA_ARGS__,BOOST_TEST_PP_RSEQ_N())
+#define BOOST_TEST_PP_NARG_(...)    BOOST_TEST_PP_ARG_N(__VA_ARGS__)
+#define BOOST_TEST_PP_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,N,...) N
+#define BOOST_TEST_PP_RSEQ_N() \
+         63,62,61,60,                   \
+         59,58,57,56,55,54,53,52,51,50, \
+         49,48,47,46,45,44,43,42,41,40, \
+         39,38,37,36,35,34,33,32,31,30, \
+         29,28,27,26,25,24,23,22,21,20, \
+         19,18,17,16,15,14,13,12,11,10, \
+         9,8,7,6,5,4,3,2,1,0
+
+// BOOST_TEST_PP_IIF(bit, t, f) -> if bit == 1, expands to t, otherwise to f
+#define BOOST_TEST_PP_IIF(bit, t, f) BOOST_TEST_PP_IIF_I(bit, t, f)
+#define BOOST_TEST_PP_IIF_I(bit, t, f) BOOST_TEST_PP_IIF_ ## bit(t, f)
+#define BOOST_TEST_PP_IIF_0(t, f) f
+#define BOOST_TEST_PP_IIF_1(t, f) t
+
+#else
+
 // Boost
 #include <boost/preprocessor/control/iif.hpp>
 #include <boost/preprocessor/comparison/equal.hpp>
 #include <boost/preprocessor/variadic/size.hpp>
+
+
+#define BOOST_TEST_PP_IIF BOOST_PP_IIF
+#define BOOST_TEST_PP_EQUAL BOOST_PP_EQUAL
+#define BOOST_TEST_PP_NARG BOOST_PP_VARIADIC_SIZE
 
 //____________________________________________________________________________//
 
@@ -33,8 +70,8 @@
 /// else:                        F2(__VA_ARGS__)
 #define BOOST_TEST_INVOKE_IF_N_ARGS( N, F1, F2, ... )               \
     BOOST_TEST_INVOKE_VARIADIC(                                     \
-        BOOST_PP_IIF(                                               \
-            BOOST_PP_EQUAL(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), N), \
+        BOOST_TEST_PP_IIF(                                               \
+            BOOST_TEST_PP_EQUAL(BOOST_TEST_PP_NARG(__VA_ARGS__), N), \
             F1,                                                     \
             F2),                                                    \
         __VA_ARGS__ )                                               \
@@ -43,6 +80,10 @@
 //____________________________________________________________________________//
 
 #endif  /* BOOST_PP_VARIADICS */
+
+
+#endif /* BOOST_TEST_PP_IMPL */
+
 
 #endif // BOOST_TEST_PP_VARIADIC_HPP_021515GER
 
