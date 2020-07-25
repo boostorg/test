8c06e2e3ade36acfc3f451b3f5ca55685a37ac6f
Raffi Enficiaud <raffi.enficiaud@free.fr>
2020-07-25 12:47:39 +0200

    WIP

---
+++ test/utils-ts/helper-macros-test.cpp
@@ -0,0 +1,23 @@
+//  (C) Copyright Raffi Enficiaud 2020.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+
+// Checks the macro indirections
+
+#define BOOST_TEST_MODULE test_helper_macro
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+#include <boost/test/results_collector.hpp>
+#include <boost/test/unit_test_suite.hpp>
+#include <boost/test/framework.hpp>
+#include <boost/test/unit_test_parameters.hpp>
+#include <boost/test/utils/nullstream.hpp>
+
+
+BOOST_AUTO_TEST_CASE( some_test )
+{
+
+}
