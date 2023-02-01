78aa24fd2444d6c354c72a58d2f24f80ed0b86d0
Matt Borland <matt@mattborland.com>
2023-02-01 11:48:50 -0800

    Workaround for MSVC

--- include/boost/test/utils/setcolor.hpp
+++ include/boost/test/utils/setcolor.hpp
@@ -90,8 +90,11 @@ public:
                           state* /* unused */= NULL)
     : m_is_color_output(is_color_output)
     {
-        m_command_size = boost::core::snprintf( m_control_command, 
-          sizeof(m_control_command), 
+        #ifdef BOOST_MSVC
+        m_command_size = std::sprintf( m_control_command,
+        #else
+        m_command_size = boost::core::snprintf( m_control_command, sizeof(m_control_command), 
+        #endif
           "%c[%c;3%c;4%cm",
           0x1B,
           static_cast<char>(attr + '0'),
@@ -103,8 +106,11 @@ public:
                          state* /* unused */)
     : m_is_color_output(is_color_output)
     {
-        m_command_size = boost::core::snprintf(m_control_command, 
-          sizeof(m_control_command), 
+        #ifdef BOOST_MSVC
+        m_command_size = std::sprintf( m_control_command, 
+        #else
+        m_command_size = boost::core::snprintf(m_control_command, sizeof(m_control_command), 
+        #endif
           "%c[%c;3%c;4%cm",
           0x1B,
           static_cast<char>(term_attr::NORMAL + '0'),
